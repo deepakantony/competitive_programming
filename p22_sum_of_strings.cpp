d71470b391ae51074ba0ce2db810bf96c04d86fd
U-beast-PC\beast <beast@beast-PC.(none)>
2011-02-20 05:05:59 -0800

    solved p22

--- p22_sum_of_strings.cpp
+++ p22_sum_of_strings.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <fstream>
 
 using namespace std;
 
@@ -8,19 +12,57 @@ int string_number(string str) {
 	while(it != str.end()) {
 		if(isalpha(*it))
 			sum = sum + (tolower(*it) - 'a' + 1);
+		++it;
 	}
 	
 	return sum;
 }
 
-template<class T, class Compare>
-void sort(T begin, T end, Compare cmp) {
 
+typedef vector<string>::const_iterator T;
+long long unsigned int sum_of_string_number(T left, T right) {
+	long long unsigned int sum = 0;
+	
+	int idx = 1;
+	for(T it = left; it != right; it++, idx++) {
+		sum += (idx * string_number(*it));
+		cout << idx << " " << *it << " " << string_number(*it) << endl;
+	}
+	
+	return sum;
 }
 
-int sum_of_string_number()
+vector<string> construct_vector(ifstream &in) {
+	char c;
+	vector<string> res;	
+	string cur_string;
+	bool start_string = false;
+	while(in.good()){
+		c = in.get();
+		if(c == '"')
+			if(!start_string)
+				start_string = true;
+			else {
+				res.push_back(cur_string);
+				cur_string = "";
+				start_string = false;
+			}
+		else if(isalpha(c) && start_string) {
+			cur_string += c;
+		}
+	}
+	return res;
+}
 
 int main(int argc, char **argv) {
+	ifstream in(argv[1]);
+	
+	vector<string> vec = construct_vector(in);
+	sort(vec.begin(), vec.end());
+	cout << sum_of_string_number(vec.begin(), vec.end()) << endl;
+	
+	in.close();
+	
 	return 0;
 }
 	
