cf9fb67861446970c42b1af6f16a49291a0d6c57
deepakantony <deepakantony@gmail.com>
2011-02-22 18:19:39 -0800

    temp mods to p23

--- p23_abundant_numbers.cpp
+++ p23_abundant_numbers.cpp
@@ -1,14 +1,39 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
+int sum_of_divisors(int n) {
+	int sum = 1;
+	
+	for(int i = 2; i*i > n && n > 1; i+=2) {
+		
+	}
+	
+	// 1 prime remains 
+	if(n > 1)
+		sum *= (n+1);
+	
 
+	return (sum-n); // for sum of proper divisors subtract n
+}
 
-bool is_sum_of_2_abundant(int n) {
+bool* build_abundant_numbers(int n) {
+	bool *abundant_numbers = (bool*)malloc(n*sizeof(bool));
+	for(int i = 0; i < 12 && i < n; i++)
+		abundant_numbers[i] = false;
+	
+	for(int i = 12; i < n; i++)
+		abundant_numbers[i] = (sum_of_divisors(i) > i) ? true : false;
+		
+	return abundant_numbers;
+}
+
+bool is_sum_of_2_abundant(int n, const bool* abundant_numbers) {
 	int n_half = floor(n/2);
 	for(int i = 12; i <= n_half; i++)
 		for(int j = n_half; j < n && (i+j) <= n; j++)
-			if(is_abundant(i) && is_abundant(j) && (i+j) == n)
+			if(abundant_numbers[i-1] && abundant_numbers[j-1] && (i+j) == n)
 				return true;
 	return false;
 }
@@ -16,11 +41,10 @@ bool is_sum_of_2_abundant(int n) {
 int main(int argc, char **argv) {
 	int sum = 23*24/2; // 24 is the smallest number that 
 					// can be written as sum of 2 abundant numbers
-	
-	for(int i = 25; i <= 28123; i++) {
-		if(is_sum_of_2_abundant(i))
+	bool* abundant_numbers = build_abundant_numbers(28123);
+	for(int i = 25; i <= 28123; i++)
+		if(is_sum_of_2_abundant(i, abundant_numbers))
 			sum += i;
-	}
 	
 	cout << sum << endl;
 
