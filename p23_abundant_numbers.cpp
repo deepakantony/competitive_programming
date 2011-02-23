45141414499765c3f83de4edb1e55f83cd060ed4
deepakantony <deepakantony@gmail.com>
2011-02-23 14:03:38 -0800

    Solved p23

--- p23_abundant_numbers.cpp
+++ p23_abundant_numbers.cpp
@@ -1,50 +1,69 @@
 #include <iostream>
 #include <cstdlib>
+#include <cmath>
+#include <vector>
 
 using namespace std;
 
 int sum_of_divisors(int n) {
 	int sum = 1;
+	int store_n = n;
 	
-	for(int i = 2; i*i > n && n > 1; i+=2) {
-		
+	for(int i = 2; i*i <= n && n > 1; i+=2) {
+		if(n % i == 0) {
+			
+			n = n/i;
+			int j = (i*i);
+			
+			while(n%i == 0) {
+				n = n/i;
+				j *= i;
+			}			
+			
+			j = (j-1)/(i-1);
+			sum *= j;
+		}
+	
+		if(i == 2) i--;
 	}
 	
 	// 1 prime remains 
 	if(n > 1)
 		sum *= (n+1);
-	
 
-	return (sum-n); // for sum of proper divisors subtract n
+	return (sum-store_n); // for sum of proper divisors subtract n
 }
 
-bool* build_abundant_numbers(int n) {
-	bool *abundant_numbers = (bool*)malloc(n*sizeof(bool));
-	for(int i = 0; i < 12 && i < n; i++)
+
+vector<bool> build_sum_of_abundant_numbers(int n) {
+	bool *abundant_numbers = new bool[n];
+	vector<bool> sum_of_abundant_numbers(n, false);
+	
+	for(int i = 0; i < 11 && i < n; i++)
 		abundant_numbers[i] = false;
 	
-	for(int i = 12; i < n; i++)
-		abundant_numbers[i] = (sum_of_divisors(i) > i) ? true : false;
+	for(int i = 11; i < n; i++) {
+		abundant_numbers[i] = (sum_of_divisors(i+1) > (i+1)) ? true : false;
+		if(abundant_numbers[i]) {
+			for(int j = 11; j <= i; j++){
+				if(abundant_numbers[j] && (i+j+1) < n)
+					sum_of_abundant_numbers[i+j+1] = true;
+			}
+		}
+	}
 		
-	return abundant_numbers;
-}
-
-bool is_sum_of_2_abundant(int n, const bool* abundant_numbers) {
-	int n_half = floor(n/2);
-	for(int i = 12; i <= n_half; i++)
-		for(int j = n_half; j < n && (i+j) <= n; j++)
-			if(abundant_numbers[i-1] && abundant_numbers[j-1] && (i+j) == n)
-				return true;
-	return false;
+	return sum_of_abundant_numbers;
 }
 
 int main(int argc, char **argv) {
 	int sum = 23*24/2; // 24 is the smallest number that 
 					// can be written as sum of 2 abundant numbers
-	bool* abundant_numbers = build_abundant_numbers(28123);
-	for(int i = 25; i <= 28123; i++)
-		if(is_sum_of_2_abundant(i, abundant_numbers))
-			sum += i;
+	int N = 28123;
+	vector<bool> sum_of_abundant_numbers = build_sum_of_abundant_numbers(N);
+	for(int i = 24; i < N; i++) {
+		if(!sum_of_abundant_numbers[i])
+			sum += (i+1);
+	}
 	
 	cout << sum << endl;
 
