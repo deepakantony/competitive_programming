fb03932041d1a6a1c9fc04f0c17df0731d5c5390
deepakantony <deepakantony@gmail.com>
2011-03-09 18:17:07 -0800

    started p37

---
+++ p37_truncatable_primes.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+bool is_prime(int n) {
+	if(n == 2 || n == 3 || n == 5 || n == 7) return true;
+	if(n%2 == 0 || n%3 == 0) return false;
+	int sqrt_n = ceil(sqrt(n));
+	for(int i = 5; i <= sqrt_n; i+=6) {
+		if(n%i == 0) return false;
+		if(n%(i+2) == 0) return false;
+	}
+	return true;
+}
+
+bool is_trancatable_prime(int n) {
+	while(n > 0) {
+		if(!is_prime(n)) return false;
+		n /= 10;
+	}
+	return true;
+}
+
+int reverse(int n) {
+	int rev = 0;
+	while(n > 0) {
+		rev = rev*10 + (n%10);
+		n /= 10;
+	}
+	return rev;
+}
+
+void find_sum(int n, int &sum) {
+	int cur = n*10+1;
+	if(is_prime(cur) && is_prime(reverse(cur)))
+		find_sum(cur, sum);
+	cur = n*10+3;
+	if(is_prime(cur) && is_trancatable_prime(reverse(cur))) {
+		sum += cur;
+		cout << cur << endl;
+		find_sum(cur, sum);
+	}
+	cur = n*10+7;
+	if(is_prime(cur) && is_trancatable_prime(reverse(cur))) {
+		sum += cur;
+		cout << cur << endl;
+		find_sum(cur, sum);
+	}
+	cur = n*10+9;
+	if(is_prime(cur) && is_prime(reverse(cur)))
+		find_sum(cur, sum);
+}
+
+int sum_of_truncatable_primes() {
+	int sum = 0;
+	find_sum(3, sum);
+	find_sum(7, sum);
+	return sum;
+}
+
+int main(int argc, char **argv) {
+	cout << sum_of_truncatable_primes() << endl;
+	cout << is_prime(973) << endl;
+	return 0;
+}
