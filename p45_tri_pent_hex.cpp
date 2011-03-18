35fd431d28fb58736ea441ae31f50ab04bd1b4de
Deepak Antony <deepakantony@Deepak-Antonys-MacBook-Pro.local>
2011-03-17 23:57:19 -0700

    solved p45

--- p45_tri_pent_hex.cpp
+++ p45_tri_pent_hex.cpp
@@ -4,31 +4,34 @@
 
 using namespace std;
 
-bool is_triangle(int n) {
-  int sqrt_n = floor(sqrt(2*n));
+typedef long long unsigned int vlong;
+
+bool is_triangle(vlong n) {
+  vlong sqrt_n = floor(sqrt(2*n));
   return (sqrt_n*(sqrt_n+1) == 2*n);
 }
 
-bool is_pentagonal(int n) {
+bool is_pentagonal(vlong n) {
   double sqrt_n = sqrt(1+24*n);
   if(ceil(sqrt_n) != floor(sqrt_n)) return false;
-  return ((int)floor(sqrt_n)+1)%6 == 0;
+  return ((vlong)floor(sqrt_n)+1)%6 == 0;
 }
 
-bool is_hexagonal(int n) {
+bool is_hexagonal(vlong n) {
   double sqrt_n = sqrt(1+8*n);
   if(ceil(sqrt_n) != floor(sqrt_n)) return false;
-  return ((int)floor(sqrt_n)+1)%4 == 0;
+  return ((vlong)floor(sqrt_n)+1)%4 == 0;
 }
 
-int H(int n) {
+vlong H(vlong n) {
   return n*(2*n-1);
 }
 
-int next_tri_pent_hex(){
-  int hex;
-  for(int i = 144; (hex=H(i)) < INT_MAX; i++){
-    if(is_pentagonal(hex) && is_triangle(hex))
+vlong next_tri_pent_hex(){
+  vlong hex;
+  for(vlong i = 144; i < INT_MAX-1; i++){
+    hex = H(i);
+    if(is_pentagonal(hex)) // a hexagonal number is a triangle number
       return hex;
   }
   return 0;
