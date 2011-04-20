4a8c7c63b6b6effb43b863cd495fc3da90e8c7c9
deepakantony <deepakantony@gmail.com>
2011-04-20 14:06:54 -0700

    solved p64

--- p64_square_root.cpp
+++ p64_square_root.cpp
@@ -18,32 +18,39 @@ vector<int> irrational_square_root(const int &n) {
 
   vector<int> res;
   res.push_back(floor(sqrt(double(n))));
-
-  int term2 = res[0];
+  int term2 = 0;
   int denom = 1;
 
   do {
-    int new_denom = (n-(term2*term2))/denom;
-    if(new_denom == 1)
-      res.push_back(term2 + res[0]);
-    else
-      res.push_back(ceil((double)term2/(double)new_denom));
-    term2 = new_denom - term2%new_denom;
+    int new_term2 = denom*(*(res.end()-1)) - term2;
+    int new_denom = (n-(new_term2*new_term2))/denom;
+    res.push_back((res[0]+new_term2)/new_denom);
+    term2 = new_term2;
     denom = new_denom;
-  }while(term2 != res[0]);
+    //cout << term2 << " " << denom <<  " : " << res[0] << endl;
+  }while(term2 != res[0] || denom != 1);
 
   return res;
 }
 
 int main(int argc, char **argv) {
   int limit = atoi(argv[1]);
+  int count = 0;
+  for(int i = 1; i <= limit; i++) {
+    if(!is_square(i)) {
+      vector<int> fraction_rep = irrational_square_root(i);
 
-  vector<int> fraction_rep = irrational_square_root(limit);
+      if((fraction_rep.size()-1)%2) {
+	count++;
+	cout << "[" << fraction_rep[0] << ";(";
+	copy(fraction_rep.begin()+1, fraction_rep.end()-1,
+	     ostream_iterator<int>(cout, ","));
+	cout << *(fraction_rep.end()-1) << ")]" << endl;
+      }
+    }
+  }
 
-  cout << "[" << fraction_rep[0] << ";(";
-  copy(fraction_rep.begin()+1, fraction_rep.end()-1,
-       ostream_iterator<int>(cout, ","));
-  cout << *(fraction_rep.end()-1) << ")]" << endl;
+  cout << count << endl;
 
   return 0;
 }
