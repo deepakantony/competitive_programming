5c05b0cebd1cb5d01ef6c201d1e538c6b4261d73
Deepak <deepakantony@gmail.com>
2011-05-26 00:15:22 -0700

    almost done

--- p66_diophantine.cpp
+++ p66_diophantine.cpp
@@ -2,6 +2,8 @@
 #include <cstdlib>
 #include <vector>
 #include <cmath>
+#include <algorithm>
+#include <iterator>
 
 using namespace std;
 
@@ -9,22 +11,70 @@ typedef unsigned long long vlong;
 
 bool is_square(int n) {
   double sqrt_n = sqrt((double)n);
-  return (ceil(n) == floor(n));
+  return (ceil(sqrt_n) == floor(sqrt_n));
+}
+
+void cf_square_root(int D, vector<int> &A) {
+  int sqrt_D = floor(sqrt(D));
+  A.push_back(sqrt_D);
+  int term2 = 0;
+  int denom = 1;
+
+  do {
+    int new_term2 = denom*(*(A.end()-1)) - term2;
+    int new_denom = (D-(new_term2*new_term2))/denom;
+    A.push_back((A[0]+new_term2)/new_denom);
+    term2 = new_term2;
+    denom = new_denom;
+  }while(term2 != A[0] || denom != 1);
 }
 
 bool solve_diophantine(int D, int &x, int &y) {
   vector<int> A; // continued fraction of square root of D
   cf_square_root(D, A);
 
+  //  cout << "A: ";
+  //  copy(A.begin(), A.end(), ostream_iterator<int>(cout, " " ));
+  //  cout << endl;
+
   // we will use pells equation to solve diophantine equation
   // let r be period(A) -1 which is the A.size() -2
   // (x,y) = (p(r), q(r)) for r being odd
   // (x,y) = (p(2r+1), q(2r+1)) when r is even
 
-  // Few adjustments before we commence
+  int pn, pn_1, pn_2, qn, qn_1, qn_2;
+  pn_2 = A[0];
+  pn = pn_1 = A[0]*A[1] + 1;
+  qn_2 = 1;
+  qn = qn_1 = A[1];
 
+  int r = A.size()-2;
+  int j = 0;
+  do {
+    for(int i = 2; i < A.size(); i++) {
+      pn_2 = pn_1;
+      pn_1 = pn;
 
-  int r = A.size()-2; 
+      qn_2 = qn_1;
+      qn_1 = qn;
+
+      pn = A[i]*pn_1+pn_2;
+      qn = A[i]*qn_1+qn_2;
+    }
+
+    if(A.size() > 2 && r%2 == 0) {
+      pn_2 = pn_1; pn_1 = pn;
+      qn_2 = qn_1; qn_1 = qn;
+      pn = A[1]*pn_1+pn_2;
+      qn = A[1]*qn_1+qn_2;
+    }
+
+    j++; 
+  } while(j < 2 && r%2 == 0);
+
+  x = pn_1; y = qn_1;
+
+  return true;
 }
 
 int main(int argc, char **argv) {
@@ -37,6 +87,7 @@ int main(int argc, char **argv) {
     if(!is_square(i)) {
       int x,y;
       solve_diophantine(i,x,y); // D = i;
+      cout << i << " " << x << " " << y << endl;
       if( x > res_x) {
 	res_x = x;
 	res_D = i;
