d9d2408c649e6d9d1cefe5e08b651563b7afa43f
Deepak <deepakantony@gmail.com>
2011-05-30 02:56:18 -0700

    solved p66

--- p66_diophantine.cpp
+++ p66_diophantine.cpp
@@ -11,7 +11,7 @@ using namespace std;
 string to_string(int n) {
   string res;
   while(n > 0) {
-    res += (n%10);
+    res += (n%10 + '0');
     n/=10;
   }
   return res;
@@ -21,30 +21,130 @@ class vlong {
 public:
   vlong(){}
   vlong(int n):_data(to_string(n)) {}
-  vlong(string data) : _data(data) {}
+  vlong(string data) {
+    reverse(data.begin(),data.end());
+    _data = data;
+  }
   vlong(const vlong &n) : _data(n._data) {}
 
-  vlong& operator+=(const vlong &n) { return *this; }
+  vlong& operator+=(const vlong &n) { 
+    string res;
+    string::const_iterator it_l = this->_data.begin(), l_end = _data.end();
+    string::const_iterator it_s = n._data.begin(), s_end = n._data.end();
+    if(_data.size() < n._data.size()) {
+      it_l = n._data.begin(); l_end = n._data.end();
+      it_s = _data.begin(); s_end = _data.end();
+    }
+    
+    int carry = 0;
+    int sum = 0;
+    while(it_l != l_end) {
+      if(it_s != s_end) {
+	sum += *it_s - '0';
+	it_s++;
+      }
+      sum += (*it_l + carry - '0');
+      carry = sum/10;
+      res += sum%10 + '0';
+      sum = 0;
+      it_l++;
+    }
+    while(carry > 0) {
+      res += carry%10+'0';
+      carry /= 10;
+    }
+
+    this->_data = res;
+
+    return *this; 
+  }
   vlong& operator+=(const int &n) { return *this += vlong(n); }
   const vlong operator+(const vlong &n) const { return vlong(*this)+=n; }
   const vlong operator+(const int &n) const { return vlong(n)+= *this; }
+  friend const vlong operator+(const int &num, const vlong &n);
 
-  vlong& operator*=(const vlong &n) { return *this; }
+  vlong& operator*=(const vlong &n) { 
+    string res;
+
+    int i = 0;
+    for(string::const_iterator it = _data.begin(); it != _data.end(); it++, i++) {
+      int k = i;
+      int carry = 0;
+      for(string::const_iterator it_s = n._data.begin(); it_s != n._data.end(); it_s++, k++) {
+	int prod = (*it-'0') * (*it_s-'0') + carry;
+	if(k < res.size()) {
+	  prod += res[k]-'0';
+	  res[k] = prod%10 + '0';
+	}
+	else {
+	  res += prod%10 + '0';
+	}
+	carry = prod/10;
+      }
+      while(carry > 0) {
+	if(k < res.size()) {
+	  carry += res[k]-'0';
+	  res[k] = carry%10 + '0';
+	}
+	else
+	  res += carry%10 + '0';
+
+	carry /= 10;
+      }
+    }
+    this->_data = res;
+
+    return *this; 
+  }
   vlong& operator*=(const int &n) { return *this *= vlong(n); }
   const vlong operator*(const vlong &n) const { return vlong(*this)*=n; }
   const vlong operator*(const int &n) const { return vlong(n)*= *this; }
+  friend const vlong operator*(const int &num, const vlong &n);
 
-  bool operator>(const vlong &n) const { return true; }
+  bool operator>(const vlong &n) const {
+    if(_data.size() > n._data.size())
+      return true;
+    else if(_data.size() < n._data.size())
+      return false;
+    for(int i = 0; i < _data.size(); i++) 
+      if(_data[i] > n._data[i])
+	return true;
+      else if(_data[i] < n._data[i])
+	return false;
+
+    return false;
+  }
   bool operator>(const int &n) const { return *this > vlong(n); }
+
+  vlong& operator=(const vlong &n) {
+    _data.clear();
+    _data = n._data;
+    return *this;
+  }
+
+  vlong& operator=(const int &n) {
+    _data.clear();
+    _data = to_string(n);
+    return *this;
+  }
 
   friend ostream& operator<<(ostream &out, const vlong &n);
 private:
   string _data;
 };
 
+const vlong operator+(const int &num, const vlong &n) {
+  return vlong(num) += n;
+}
+
+const vlong operator*(const int &num, const vlong &n) {
+  return vlong(num) *= n;
+}
+
 ostream& operator<<(ostream &out, const vlong &n) {
-  for(string::const_iterator it = n._data.begin(); it != n._data.end(); it++)
-    out << *it + '0';
+  string data(n._data);
+  reverse(data.begin(), data.end());
+  cout << data;
   return out;
 }
 
@@ -81,8 +181,9 @@ bool solve_diophantine(int D, vlong &x, vlong &y) {
   // (x,y) = (p(r), q(r)) for r being odd
   // (x,y) = (p(2r+1), q(2r+1)) when r is even
 
-  int pn, pn_1, pn_2, qn, qn_1, qn_2;
+  vlong pn, pn_1, pn_2, qn, qn_1, qn_2;
   pn_2 = A[0];
+
   pn = pn_1 = A[0]*A[1] + 1;
   qn_2 = 1;
   qn = qn_1 = A[1];
@@ -121,6 +222,7 @@ bool solve_diophantine(int D, vlong &x, vlong &y) {
 }
 
 int main(int argc, char **argv) {
+
   vlong res_D = 0;
   vlong res_x = 0;
 
