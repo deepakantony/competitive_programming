2ed072c0747d53eab13a02ac821586cce8383e3d
Deepak <deepakantony@gmail.com>
2011-09-13 00:09:17 -0700

    started 5 gon ring problem

---
+++ p68_magic_gon_ring/NGonRing.h
@@ -0,0 +1,10 @@
+#ifndef _NGONRING_H_
+#define _NGONRING_H_
+
+class NGonRing 
+{
+	
+};
+
+#endif // _NGONRING_H_
+
---
+++ p68_magic_gon_ring/NGonRingMain.cpp
@@ -0,0 +1,8 @@
+#include "NGonRing.h"
+#include <iostream>
+
+int main(int argc, char *argv[]) 
+{
+	return 0;
+}
+
