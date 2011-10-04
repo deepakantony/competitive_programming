55b97ecd89f5b98fbca962e8a5c52422a21bf566
Deepak Antony <deepakantony@gmail.com>
2011-10-03 18:13:45 -0700

    slowly adding the brute force solution for this problem

--- p68_magic_gon_ring/NGonRing.cpp
+++ p68_magic_gon_ring/NGonRing.cpp
@@ -1,6 +1,7 @@
 #include "NGonRing.h"
 #include <vector>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -9,6 +10,7 @@ using namespace std;
 NGonRing::NGonRing(int nGon)
 	:_nGon(nGon)
 {
+	_totalDigits = nGon*2;
 }
 
 
@@ -18,8 +20,77 @@ bool NGonRing::generateSolutions(
 	int totalPerLine, vector<string> &nGonRingSolutions
 	) const
 {
+	vector<int> possibleSolutions;
+	for(int entry = 1; entry <= _nGon; entry++)
+		possibleSolutions.push_back(entry);
+
+	do
+	{
+		if (isValidSolution(possibleSolutions))
+		{
+			nGonRingSolutions.push_back(
+				convertVectorToString(possibleSolutions)
+				);
+		}
+	}
+	while(
+		next_permutation(possibleSolutions.begin(), possibleSolutions.end())
+		);
+	
 	return true;
 }
+
+//
+// Generate all solutions 
+bool NGonRing::generateSolutions( vector<string> &nGonRingSolutions	) const
+{
+	// The lowest & highest total of the NGonRing
+	int lowestTotal = _nGon/2 + (_nGon/2 - 1) + (_nGon/2 + 1);
+	int highestTotal = lowestTotal + 2;
+
+	for (int total = lowestTotal; total <= highestTotal; total++)
+	{
+		generateSolutions(total, nGonRingSolutions);
+	}
+
+	return true;
+}
+
+//
+// is the solution a valid solution
+bool NGonRing::isValidSolution( const vector<int> &solution ) const
+{
+	if(solution.size() > _nGon*3)
+		return false;
+
+	return true;
+}
+
+//
+// Convert the vector of integers to string
+string convertVectorToString(const std::vector<int> &solutionVector)
+{
+	string res;
+	for(vector<int>::const_iterator it = solutionVector.begin();
+		it != solutionVector.end(); it++)
+		res += int2String(*it);
+	return res;
+}
+
+// 
+// Convert int to string
+string int2String(int n)
+{
+	string res;
+	while(n>0)
+	{
+		res += ('0' + n%10);
+		n /= 10;
+	}
+	reverse(res.begin(), res.end());
+	return res;
+}
+
 
 // 
 // Our special display function
--- p68_magic_gon_ring/NGonRing.h
+++ p68_magic_gon_ring/NGonRing.h
@@ -7,23 +7,36 @@
 class NGonRing 
 {
 public:
-	// constructor
-	NGonRing(int nGon);
+    // constructor
+    NGonRing(int nGon);
 	
-	// Generate solutions for the total requested
-	bool generateSolutions(
+    // Generate solutions for the total requested
+    bool generateSolutions(
 		int totalPerLine, 
 		std::vector<std::string> &nGonRingSolutions
 		) const; 
+    
+    // Generate all solutions
+    bool generateSolutions(
+		std::vector<std::string> &nGonRingSolutions
+		) const; 
+
+	// Is the solution a valid solution
+	bool isValidSolution( const std::vector<int> &solution ) const;
 
 private:
-	int _nGon; // size of this ring
+    int _nGon; // size of this ring
+	int _totalDigits; // total number of digits
 };
 
+std::string convertVectorToString(const std::vector<int> &solutionVector);
+
+std::string int2String(int n);
+
 bool displaySolutions(
-	int nGon, int totalPerLine, 
-	const std::vector<std::string> &nGonRingSolutions
-	);
+    int nGon, int totalPerLine, 
+    const std::vector<std::string> &nGonRingSolutions
+    );
 
 #endif // _NGONRING_H_
 
