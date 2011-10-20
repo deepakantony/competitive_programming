f941b0feeff7da16b7b60f2eb280c4d7b5a71cbd
Deepak <deepakantony@gmail.com>
2011-10-19 22:34:25 -0700

    renamed totientratio to totientratiomain

--- p69_totient_function/TotientFunction.h
+++ p69_totient_function/TotientFunction.h
@@ -1,6 +1,14 @@
 #ifndef _TOTIENT_FUNCTION_H_
 #define _TOTIENT_FUNCTION_H_
 
+class SievePrimeList 
+{
+	typedef unsigned int uint;
+public:
+	SievePrimeList(int size);
+
+
+};
 
 
 #endif // _TOTIENT_FUNCTION_H_
