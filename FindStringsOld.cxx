370b509d25260f61ec69ddcdc7cbb329ac7a7e8a
Deepak Antony <deepakantony@gmail.com>
2012-03-07 18:21:51 -0800

    not working linear suffix array implementation

--- FindStringsOld.cxx
+++ FindStringsOld.cxx
@@ -8,27 +8,35 @@
 using namespace std;
 
 // Convert string to char; but normalized (so subtract 'a' from it; start from 1)
-void convertStringToIntChar(const string &str, char *output)
+void convertStringToIntChar(const string &str, int *output)
 {
-	output = new char[str.size()];
 	int index = 0;
-	for(auto it = str.begin(); it != str.end(); ++it)
-		output[index++] = *it - 'a' + 1;
+	for(auto it = str.begin(); it != str.end(); ++it) 
+	{
+		output[index++] = 1 + (int)(*it) - (int)'a';
+#ifdef _DBUG
+		cout << *it << "," << index-1;
+#endif
+	}
+#ifdef _DBUG
+	cout << endl;
+#endif
+	output[str.size()] = output[str.size()+1] = output[str.size()+2] = 0;
 }
 
 // using the source code from the paper: Linear Work suffix array construction
 
 // Lexicographic order for pairs
-template<class pair_type>
-bool leq(pair_type a1, pair_type a2, pair_type b1, pair_type b2)
+template<class pair_type1, class pair_type2>
+bool leq(pair_type1 a1, pair_type2 a2, pair_type1 b1, pair_type2 b2)
 {
 	return (a1 < b1 || (a1 == b1 && a2 < b2));
 }
 
 // Lexicographic order for triplets
-template<class triplet_type>
-bool leq(triplet_type a1, triplet_type a2, triplet_type a3,
-		 triplet_type b1, triplet_type b2, triplet_type b3)
+template<class triplet_type1, class triplet_type2>
+bool leq(triplet_type1 a1, triplet_type1 a2, triplet_type2 a3,
+		 triplet_type1 b1, triplet_type1 b2, triplet_type2 b3)
 {
 	return (a1 < b1 || (a1 == b1 && leq(a2, a3, b2, b3)));
 }
@@ -63,7 +71,8 @@ void radixPass(index_type *array, index_type *output,
 {
 	// initialize counter
 	// counter = {0, 0, 0, 0, 0} // assuming K = 4 for only a, b, c and d
-	index_type *counter = new index_type[K+1](0);
+	index_type *counter = new index_type[K+1];
+	fill(counter, counter+K+1, 0);
 
 	// Count occurences
 	// counter = {0, 3, 2, 0, 1}
@@ -78,7 +87,7 @@ void radixPass(index_type *array, index_type *output,
 	for(auto index = 0, sum = 0; index <= K; ++index)
 	{
 		auto temp = counter[index];
-		counter = sum;
+		counter[index] = sum;
 		sum += temp;
 	}
 
@@ -144,6 +153,9 @@ void constructSuffixArray(element_type *inputArray, index_type *suffixArray,
 		if( index%3 != 0)
 			s12[indexS12++] = index; 
 
+
+	cout << "First radix pass" << endl;
+
 	// Sort the triplets
 	// For our example...
 	radixPass(s12, SA12, inputArray + 2, n02, K);
@@ -151,10 +163,12 @@ void constructSuffixArray(element_type *inputArray, index_type *suffixArray,
 	// { 3 4 6 7 9 10 12 13 } is the actual indices in inputArray that is 
 	// compared. the alphabets compared are { b a a b a d 0 0 }
 	// SA12 = { 12 13 4 6 9 3 7 10 } - 2 = { 10 11 2 4 7 1 5 8 } 
+	cout << "Second radix pass" << endl;
 	radixPass(SA12, s12, inputArray + 1, n02, K);
 	// second radix pass we're comparing SA12 + 1 = { 11 12 3 5 8 2 6 9 }
 	// the alphabets compared are { o 0 b d b b a a }
 	// so s12 = { 12 6 9 3 8 2 5 11 } - 1 = { 11 5 8 2 7 1 4 10 }
+	cout << "Third radix pass" << endl;
 	radixPass(s12, SA12, inputArray, n02, K);
 	// third radix pass we're comparing s12 = { 11 5 8 2 7 1 4 10 }
 	// the alphabets compared are { o d b b b a a d }
@@ -165,7 +179,8 @@ void constructSuffixArray(element_type *inputArray, index_type *suffixArray,
 	// So at the end of this loop name = 7, since there is duplicate
 	// triplet [bba]
 	int name = 0;
-	element_type *prev = new element_type[3](-1);
+	element_type *prev = new element_type[3];
+	fill(prev, prev+2, -1);
 	for(auto index = 0; index < n02; index++)
 	{
 		if( ! tripletEquals(prev, inputArray+index) )
@@ -190,14 +205,14 @@ void constructSuffixArray(element_type *inputArray, index_type *suffixArray,
 		constructSuffixArray(s12, SA12, n02, name);
 		// store unique names in s12 using the suffix array
 		for(int index = 0; index < n02; ++index)
-			s12[SA12[i]] = i + 1;
+			s12[SA12[index]] = index + 1;
 		// our s12 will be s12 = [1 2 5 7 3 4 6 8 
 		//                        0 1 2 3 4 5 6 7
 	}
 	else
 	{ // generate the suffix array of s12 directly
 		for(int index = 0; index < n02; ++index)
-			SA12[s12[index] - 1] = i;
+			SA12[s12[index] - 1] = index;
 	}
 
 	// stably sort mod 0 suffixes from SA12 by their first character
@@ -206,33 +221,69 @@ void constructSuffixArray(element_type *inputArray, index_type *suffixArray,
 		if(SA12[index] < n0)
 			s0[indexS0++] = 3*SA12[index];
 	// after the radix pass SA0 = [ 6 9 3 0 ]
+	cout << "Fourth radix pass" << endl;
 	radixPass(s0, SA0, inputArray, n0, K);
 
+	cout << "All radix passes done" << endl;
+
 	// merge sorted SA0 suffixes and sorted SA12 suffixes
-	  for (int p=0,  t=n0-n1,  k=0;  k < n;  k++) {
-#define GetI() (SA12[t] < n0 ? SA12[t] * 3 + 1 : (SA12[t] - n0) * 3 + 2)
-    int i = GetI(); // pos of current offset 12 suffix
-    int j = SA0[p]; // pos of current offset 0  suffix
-    if (SA12[t] < n0 ? 
-        leq(s[i],       s12[SA12[t] + n0], s[j],       s12[j/3]) :
-        leq(s[i],s[i+1],s12[SA12[t]-n0+1], s[j],s[j+1],s12[j/3+n0]))
-    { // suffix from SA12 is smaller
-      SA[k] = i;  t++;
-      if (t == n02) { // done --- only SA0 suffixes left
-        for (k++;  p < n0;  p++, k++) SA[k] = SA0[p];
-      }
-    } else { 
-      SA[k] = j;  p++; 
-      if (p == n0)  { // done --- only SA12 suffixes left
-        for (k++;  t < n02;  t++, k++) SA[k] = GetI(); 
-      }
-    }  
-  }
+	for(int indexS0 = 0, indexS12 = 0, index = 0; index < n; ++index)
+	{
+		auto curPos12 = (SA12[indexS12] < n0) ?  
+			SA12[indexS12] * 3 + 1 : (SA12[indexS12] - n0) * 3 + 2;
+		auto curPos0 = SA0[indexS0];
+		if( SA12[indexS12] < n0 ? 
+			leq(inputArray[curPos12],  s12[SA12[indexS12] + n0], 
+				inputArray[curPos0], s12[curPos0/3]) :
+			leq(inputArray[curPos12], inputArray[curPos12+1], s12[SA12[indexS12]-n0+1],
+				inputArray[curPos0], inputArray[curPos0+1], s12[curPos0/3 + n0]))
+		{
+			suffixArray[index] = curPos12;
+			indexS12++;
+			if(indexS12 == n02) // done -- only SA0 suffixes left
+			{
+				for(index++; indexS0 < n0; ++indexS0, ++index)
+					suffixArray[index] = SA0[indexS0];
+			}
+		}
+		else
+		{
+			suffixArray[index] = curPos0;
+			indexS0++;
+			if(indexS0 == n0) // done -- only SA12 suffixes left
+			{
+				for(index++; indexS12 < n02; ++indexS12, ++index)
+					suffixArray[index] = (SA12[indexS12] < n0) ?  
+						SA12[indexS12] * 3 + 1 : (SA12[indexS12] - n0) * 3 + 2;
+			}
+		}			   
+	}
+
+	cout << "suffix array calculated" << endl;
 
 	delete [] s12;
 	delete [] SA12;
 	delete [] SA0;
 	delete [] s0;
+}
+
+template<class container_type>
+void printContainer(container_type c)
+{
+	cout << "( ";
+	for(auto it = c.begin(); it != c.end(); ++it)
+		cout << *it << " ,";
+	cout << " )" << endl;
+}
+
+template<class iterator_type>
+void printContainer(iterator_type left, iterator_type right)
+{
+	cout << "( ";
+	for(iterator_type it = left; it != right; ++it)
+		cout << (int)*it << ", ";
+	cout << " )" << endl;
+	
 }
 
 class SuffixArray
@@ -263,6 +314,8 @@ private:
 		long long index; // when queried this will be used to do the binary search
 	};
 
+	bool nodeLT(const SANode &node1, const SANode &node2) const;
+	bool nodeEQ(const SANode &node1, const SANode &node2) const;
 	int findLCP(const SANode &prev, const SANode &cur) const;
 	bool nodeHasStringIndex(const SANode &node, long long idx) const;
 	bool nodeIsLTIndex(const SANode &node, long long idx) const;
@@ -280,6 +333,7 @@ private:
 	vector<SANode> suffixVec; // random access
 	long long substringsMaxIndex; // number of distinct substrings - 1
 	vector<string> stringList; // list of strings and suffixs
+	vector<SANode> sortedSuffixSet; // constructed using linear suffix array algo
 
 	struct SNode // substring node
 	{
@@ -338,14 +392,14 @@ void SuffixArray::updateVector()
 void SuffixArray::updateSubstringVec()
 {
 	substringVec.clear();
-	if(suffixSet.empty())
+	if(sortedSuffixSet.empty())
 		return;
-	addAllSubstrings(*suffixSet.begin());
-	auto it = suffixSet.begin();
+	addAllSubstrings(*sortedSuffixSet.begin());
+	auto it = sortedSuffixSet.begin();
 	SANode prev = *it;
 	SANode cur;
 	++it;
-	while(it != suffixSet.end())
+	while(it != sortedSuffixSet.end())
 	{
 		cur = *it;
 		cur.lcp = findLCP(prev, cur);
@@ -430,7 +484,42 @@ void SuffixArray::updateSuffixArray(const string &inpStr)
 {
 	int curIndex = stringList.size();
 	stringList.push_back(inpStr);
-	
+	cout << inpStr << endl;
+	// convert to normalized char*
+	int *inpStrChar = new int[inpStr.size()+3];;
+	convertStringToIntChar(inpStr, inpStrChar);
+
+	printContainer(inpStrChar, inpStrChar+inpStr.size());
+
+	int *suffixArray = new int[inpStr.size()];
+	int K = 26; // 26 english chars supported
+
+	constructSuffixArray(inpStrChar,  suffixArray, (int)inpStr.size(), K); 
+	printContainer(suffixArray, suffixArray+inpStr.size());
+	vector<SANode> tempSortedSet(sortedSuffixSet.begin(), sortedSuffixSet.end());
+	sortedSuffixSet.clear();
+	cout << "Sorting and merging begins" << endl;
+	auto it_temp = tempSortedSet.begin();
+	for(auto index = 0; index < inpStr.size(); ++index)
+	{
+		SANode newNode;
+		newNode.strIdx = curIndex;
+		newNode.leftIndex = suffixArray[index];
+		
+		while(it_temp != tempSortedSet.end() && nodeLT(*it_temp, newNode))
+		{
+			sortedSuffixSet.push_back(*it_temp);
+			++it_temp;
+		}
+		if( (it_temp != tempSortedSet.end() && !nodeEQ(*it_temp, newNode)) 
+			|| (it_temp == tempSortedSet.end()))
+		{
+			sortedSuffixSet.push_back(*it_temp);
+		}
+	}
+	for(;it_temp != tempSortedSet.end(); ++it_temp)
+		sortedSuffixSet.push_back(*it_temp);
+	cout << "Sofrting and merging complete" << endl;
 /* // old sequential method
 	int curIndex = stringList.size();
 	stringList.push_back(inpStr);
@@ -487,6 +576,34 @@ string SuffixArray::constructStringInNode(const SANode &node, int prefixIndex) c
 
 	return res;
 }
+
+bool SuffixArray::nodeLT(const SANode &node1, const SANode &node2) const
+{
+	return nc(node1, node2);
+}
+
+bool SuffixArray::nodeEQ(const SANode &node1, const SANode &node2) const
+{
+	auto node1Size = stringList[node1.strIdx].size() - node1.leftIndex;
+	auto node2Size = stringList[node2.strIdx].size() - node2.leftIndex;
+
+	if(node1Size != node2Size)
+		return false;
+	else
+	{
+		for(auto it_node1 = stringList[node1.strIdx].begin()+node1.leftIndex,
+				it_node2 = stringList[node2.strIdx].begin()+node2.leftIndex;
+			it_node1 != stringList[node1.strIdx].end() && 
+				it_node2 != stringList[node2.strIdx].end(); 
+			++it_node1, ++it_node2)
+		{
+			if(*it_node1 != *it_node2)
+				return false;
+		}
+	}
+	return true;
+}
+
 
 bool SuffixArray::NodeCmp::operator()(const SANode &ths, const SANode &other) const
 {
@@ -578,7 +695,7 @@ void solveFindStrings()
 
 int main(int argc, char *argv[])
 {
-	solveFindStrings();
-//	unitTests();
+//	solveFindStrings();
+	unitTests();
 	return 0;
 }
