34091886f691a75eccbf5b0feede642697adcd03
Deepak <deepakantony@gmail.com>
2012-03-15 00:35:53 -0700

    python is also the same; however i have figured out that the online judge will stop once I exceed the time limit, so the execution time shown is not actual, it could be more

--- NonTimedChallenges/QuadrantQueries.py
+++ NonTimedChallenges/QuadrantQueries.py
@@ -1,10 +1,14 @@
-import sys
+import math
 
 def findQuadrandInfo(x, y, quadrantInfo):
     if x >= 0:
-        quadrantInfo[0] = 1 if y >= 0 else quadrantInfo[3] = 1
+        if y >= 0:
+            quadrantInfo[0] = 1 
+        else: quadrantInfo[3] = 1
     else:
-         quadrantInfo[1] = 1 if y >= 0 else quadrantInfo[2] = 1
+        if y >= 0:
+            quadrantInfo[1] = 1
+        else: quadrantInfo[2] = 1
 
 def add(q1, q2, sum):
     sum[0] = q1[0] + q2[0]
@@ -27,134 +31,108 @@ def reflectQY(q):
     q[3], q[2] = q[2], q[3]
 
 def printQuadrant(q):
-    print "%d %d %d %d\n" % q[0], q[1], q[2], q[3]
+    print "%d %d %d %d" % (q[0], q[1], q[2], q[3])
 
 def left(index):
-    return 2*index + 4;
+    return 2*index + 1
 
-def right(int index):
-    return 2*index + 8;
+def right(index):
+    return 2*index + 2
 
-def parent(int index):
-    return (index-4)/2;
+def parent(index):
+    return (index-4)/2
 
 
 class Tree:
-    def __init__(self, size):
-        self.curI = self.curJ = self.curReflect = 0
-        self.tree = 
-    curI, curJ, curReflect;
-int *tree;
-int size;
-int *q;
+    def __init__(self, _nPoints):
+        self.curI = self.curJ = 0
+        self.curReflect = ''
+        self.nPoints = _nPoints
+        self.size = int(math.ceil(math.log(self.nPoints))*self.nPoints)
+        self.tree = [ [0]*4 for x in xrange(self.size) ]
+        self.q = [0]*4
+        self.initialize(0, 0, self.nPoints-1)
 
-void initialize(int cur, int low, int high) {
-	if(low == high) {
-		int x, y;
-		scanf("%d %d", &x, &y);
-		findQuadrandInfo(x, y, tree+cur);
-	}
-	else {
-		int l = left(cur), r = right(cur), mid = (low+high)/2;
-		initialize(l, low, mid);
-		initialize(r, mid+1, high);
 
-		add(tree+l, tree+r, tree+cur);
-	}
-}
+    def initialize(self, cur, low, high):
+	if low == high :
+            (x,y) = raw_input().split()
+            findQuadrandInfo(int(x), int(y), self.tree[cur])
+	else:
+            l = left(cur)
+            r = right(cur)
+            mid = (low+high)/2
+            self.initialize(l, low, mid)
+            self.initialize(r, mid+1, high)
+            add(self.tree[l], self.tree[r], self.tree[cur])
 
-void getInfo(int cur, int low, int high) {
-	if(low == high) addTo(tree+cur, q);
-	else if(low >= curI && high <= curJ) addTo(tree+cur, q);
-	else {
-		int l = left(cur), r = right(cur), mid = (low+high)/2;
-		if(curI > mid) getInfo(r, mid+1, high);
-		else if(curJ <= mid) getInfo(l, low, mid);
-		else {
-			getInfo(r, mid+1, high);
-			getInfo(l, low, mid);
-		}
-	}
-}
+    def getInfo(self, cur, low, high):
+	if low == high:
+            addTo(self.tree[cur], self.q)
+	elif low >= self.curI and high <= self.curJ:
+            addTo(self.tree[cur], self.q)
+	else:
+            l = left(cur)
+            r = right(cur)
+            mid = (low+high)/2
+            if self.curI > mid: self.getInfo(r, mid+1, high)
+            elif self.curJ <= mid: self.getInfo(l, low, mid)
+            else:
+                self.getInfo(r, mid+1, high)
+                self.getInfo(l, low, mid)
 
-void reflectQ(int *q) {
-	if(curReflect)
-		reflectQX(q);
-	else
-		reflectQY(q);
-}
 
-void swapReflect(int cur, int low, int high) {
-	if(low == high) {
-		reflectQ(tree+cur);
-	}
-	else {
-		int mid = (low+high)/2;
-		swapReflect(left(cur), low, mid);
-		swapReflect(right(cur), mid+1, high);
-		reflectQ(tree+cur);
-	}
-}
+    def reflectQ(self, quadrant):
+	if self.curReflect: reflectQX(quadrant)
+	else: reflectQY(quadrant)
 
-void reflect(int cur, int low, int high) {
-	if(low == high) reflectQ(tree+cur);
-	else if(low >= curI && high <= curJ)
-		swapReflect(cur, low, high);
-	else {
-		int l = left(cur), r = right(cur), mid = (low+high)/2;
-		if(curI > mid) reflect(r, mid+1, high);
-		else if(curJ <= mid) reflect(l, low, mid);
-		else {
-			reflect(l, low, mid);
-			reflect(r, mid+1, high);
-		}
-		add(tree+l, tree+r, tree+cur);
-	}
-}
+    def swapReflect(self, cur, low, high):
+	if low == high:
+            self.reflectQ(self.tree[cur])
+	else:
+            mid = (low+high)/2
+            self.swapReflect(left(cur), low, mid)
+            self.swapReflect(right(cur), mid+1, high)
+            self.reflectQ(self.tree[cur])
 
-void printTree() {
-	for(int i = 0; i < size; i+=4) {
-		printf("i:%d - ", i);
-		printQuadrant(tree+i);
-	}
-}
 
-void solveQuadrantQueries()
-{
-	int nPoints;
-	scanf("%d", &nPoints);
-	size = ceil(log2(nPoints))*nPoints*4;
+    def reflect(self, cur, low, high):
+	if low == high: self.reflectQ(self.tree[cur])
+	elif low >= self.curI and high <= self.curJ:
+            self.swapReflect(cur, low, high)
+	else:
+            l = left(cur)
+            r = right(cur)
+            mid = (low+high)/2
+            if self.curI > mid: self.reflect(r, mid+1, high)
+            elif self.curJ <= mid: self.reflect(l, low, mid)
+            else:
+                self.reflect(l, low, mid)
+                self.reflect(r, mid+1, high)
 
-	tree = (int*)malloc(size*sizeof(int));
-	memset(tree, 0, size*sizeof(int));
-	initialize(0, 0, nPoints-1);
+            add(self.tree[l], self.tree[r], self.tree[cur])
 
-	int nQueries;
-	scanf("%d", &nQueries);
-	q = (int*)malloc(4*sizeof(int));
-	
-	for(int query = 0; query < nQueries; ++query)
-	{
-		char action;
-		scanf(" %c %d %d", &action, &curI, &curJ);
-		curI--; curJ--;
-		switch (action) {
-		case 'C':
-			memset(q, 0, 4*sizeof(int));
-			getInfo(0, 0, nPoints-1);
-			printQuadrant(q);
-			break;
-		case 'X':
-			curReflect = 1;
-			reflect(0, 0, nPoints-1);
-			break;
-		case 'Y':
-			curReflect = 0;
-			reflect(0, 0, nPoints-1);
-			break;
-		}
-	}
-}
+    def printTree(self):
+        for entry in self.tree:
+            print entry
+
+    def doQueries(self, nQueries):
+        for query in xrange(nQueries):
+            (self.curReflect, self.curI, self.curJ) = raw_input().split()
+            self.curI = int(self.curI) - 1
+            self.curJ = int(self.curJ) - 1
+            if self.curReflect == 'C':
+                self.q = [0, 0, 0, 0]
+                self.getInfo(0, 0, self.nPoints-1)
+                printQuadrant(self.q)
+            else:
+                self.reflect(0, 0, self.nPoints-1)
+
+def solveQuadrantQueries():
+    nPoints = int(raw_input())
+    tree = Tree(nPoints)
+    nQueries = int(raw_input())
+    tree.doQueries(nQueries)
 
 def main():
     solveQuadrantQueries();
