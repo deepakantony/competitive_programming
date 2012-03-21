1c66014b72445c4ed93aeafea90f9def77a34611
Deepak <deepakantony@gmail.com>
2012-03-21 00:07:30 -0700

    starting with the DP problem zigzag

---
+++ topcoder/ZigZag.cxx
@@ -0,0 +1,7 @@
+// TCCC '03 Semifinals 3 Division I Level One
+// http://community.topcoder.com/tc?module=ProblemDetail&rd=4493&pm=1259
+
+// 
+//  First let define the recursive solution
+//  ZZ[n][S] = ZZ[n-1][S] + ZZ
+// 
