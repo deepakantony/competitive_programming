8e83911283f46f26b0fc97f117ba8dc281ea0f4c
Deepak Antony <deepakantony@gmail.com>
2012-04-12 18:25:37 -0700

    started this problem

---
+++ uva_online/ProgrammingParadigms/DynamicProgramming/DivisibleGroupSums_10616.cxx
@@ -0,0 +1,13 @@
+
+#include <cstdio>
+
+int numbers[100];
+int memo[1001][11][21];
+int N, M;
+
+#define REP(i, st, n) for(int (i) = (st); (i) < (n); ++(i))
+
+int buildMemo(int divisor)
+{
+	if(memo[N][M][D]
+}
