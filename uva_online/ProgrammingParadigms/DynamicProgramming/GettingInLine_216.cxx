d15f7322432be2e8d48c72c160df23cad077323f
Deepak Antony <deepakantony@gmail.com>
2012-04-20 18:34:53 -0700

    cleaned up few mess; still not complete

--- uva_online/ProgrammingParadigms/DynamicProgramming/GettingInLine_216.cxx
+++ uva_online/ProgrammingParadigms/DynamicProgramming/GettingInLine_216.cxx
@@ -9,9 +9,19 @@ using namespace std;
 double dist[8][8];
 int x[8], y[8];
 double memo[8][(1<<8)-1];
-int backtrack[(1<<8)-1];
+int backtrack[8][(1<<8)-1];
 int N;
 
+bool isBitOn(int num, int bitPos)
+{
+	return (num&(1<<bitPos)) != 0;
+}
+
+int unsetBit(int num, int bitPos)
+{
+	return (num&(~(1<<bitPos)));
+}
+	
 void calculateAllDists()
 {
 	REP(i,0,N) REP(j,i,N) {
@@ -25,39 +35,37 @@ double TSP(int curCoord, int bitmask)
 {
 	if(memo[curCoord][bitmask] >= 0.0) return memo[curCoord][bitmask];
 	if(bitmask == 0) memo[curCoord][0] = 0.0;
-	else if(curCoord == -1) {
-		REP(mask, 0, N) {
-			double res = TSP(mask, bitmask&(~(1<<mask)));
-			if(memo[mask][bitmask] < 0.0 || res < memo[mask][bitmask]) {
-				memo[mask][bitmask] = res;
-				backtrack[bitmask] = mask;
-			}
-		}
-	}
 	else {
-		REP(mask, 0, N) 
-			if((bitmask&(1<<mask))) {
-				double res = dist[curCoord][mask] + 
-					TSP(mask, bitmask&(~(1<<mask)));
+		REP(pos, 0, N) {
+			if(isBitOn(bitmask, pos)) {
+				double res = dist[curCoord][pos] + 
+					TSP(pos, unsetBit(bitmask, pos));
 				if(memo[curCoord][bitmask] < 0.0 || 
 				   res < memo[curCoord][bitmask]) {
+					//			printf("Curres: %.2f Curcoord: %d bitmask: %X pos: %d curMemo %.2f\n",
+					// res, curCoord, bitmask, pos, memo[curCoord][bitmask]);
+
+
 					memo[curCoord][bitmask] = res;
-					backtrack[bitmask] = mask;
+					backtrack[curCoord][bitmask] = pos;
 				}
 			}
+		}
 	}
 	return memo[curCoord][bitmask];
 }
 
-void bt(int bitmask)
+void bt(int curCoord, int bitmask)
 {
 	if(bitmask == 0) return;
-	int c1 = backtrack[bitmask];
-	bitmask = (bitmask&(~(1<<c1)));
-	int c2 = backtrack[bitmask];
+	printf("curbitmask: %d\n", bitmask);
+	int c1 = backtrack[curCoord][bitmask];
+	int newbitmask = (bitmask&(~(1<<c1)));
+	int c2 = backtrack[c1][newbitmask];
 	printf("Cable requirement to connect (%d,%d) to (%d,%d) is %.2f feet.\n",
 		   x[c1], y[c1], x[c2], y[c2], dist[c1][c2]);
-	bt(bitmask);
+	if( bitmask != newbitmask)
+		bt(c1, newbitmask);
 }
 
 void clearmemo() {
@@ -74,7 +82,9 @@ int main(int argc, char *argv[])
 		REP(i, 0, N) scanf(" %d %d", &x[i], &y[i]);
 		calculateAllDists();
 		clearmemo();
-		res = TSP(-1, ((1<<N)-1));
+		res = 2*151*151;
+		REP(pos, 0, N)
+			res = min(res, TSP(pos, unsetBit(((1<<N)-1), pos)));
 		printf("**********************************************************\n");
 		printf("Network #%d\n", test);
 		//bt((1<<N)-1);
