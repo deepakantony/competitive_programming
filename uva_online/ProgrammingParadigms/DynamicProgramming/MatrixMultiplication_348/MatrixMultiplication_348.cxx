6aa2064462d4e51ed19d26836e906a6b4d77ef9f
Deepak <deepakantony@gmail.com>
2012-05-24 00:42:29 -0700

    solved with simple DP

--- uva_online/ProgrammingParadigms/DynamicProgramming/MatrixMultiplication_348/MatrixMultiplication_348.cxx
+++ uva_online/ProgrammingParadigms/DynamicProgramming/MatrixMultiplication_348/MatrixMultiplication_348.cxx
@@ -1,22 +1,21 @@
 #include <cstdio>
 #include <cstring>
+#include <ctime>
 
 #define FOR(i, st, n) for(int (i) = (st), _n = (n); (i) < _n; ++(i))
 
 typedef unsigned long long LL;
 
-LL matrix[101][2];
-LL memo[101][101];
-LL row[101][101];
-LL col[101][101];
-LL pos[101][101];
+int matrix[11][2];
+LL memo[11][11];
+int pos[11][11];
 
 
 // memo(l, r) = 0 if (l+1 == r)
 //            = min(memo(l, l+i) + memo(l+i, r) + 
 //               row(l) * row(l+i) * col(r-1) 
 //               for all i -> [l+1, r))
-int optimalMult(int left, int right) 
+LL optimalMult(int left, int right) 
 {
 	if(right > left && memo[left][right] == -1ll) {
 		if(right == (left+1)) {
@@ -24,13 +23,14 @@ int optimalMult(int left, int right)
 			pos[left][right] = 0;
 		}
 		else {
-			FOR(pos, left+1, right) {
+			FOR(position, left+1, right) {
 				LL curVal = 
-					optimalMult(left, pos) + optimalMult(pos, right) +
-					matrix[left][0] * matrix[pos][0] * matrix[right-1][1];
+					optimalMult(left, position) + 
+					optimalMult(position, right) +
+					matrix[left][0] * matrix[position][0] * matrix[right-1][1];
 				if(curVal < memo[left][right]) {
 					memo[left][right] = curVal;
-					pos[left][right] = pos;
+					pos[left][right] = position;
 				}
 			}
 		}
@@ -38,4 +38,47 @@ int optimalMult(int left, int right)
 	return memo[left][right];
 }
 
+// backtrack printing
+void printMultiplySeq(int left, int right)
+{
+	if(right == (left+1))
+		printf("A%d", right);
+	else {
+		printf("(");
+		printMultiplySeq(left, pos[left][right]);
+		printf(" x ");
+		printMultiplySeq(pos[left][right], right);
+		printf(")");
+	}
+}
 
+void printPos(int nMatrices) {
+	FOR(row, 0, nMatrices) {
+		FOR(col, 0, nMatrices) 
+			printf ("%d ", pos[row][col]);
+		printf("\n");
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	clock_t begin = clock();
+	int nMatrices;
+	int nCase = 1;
+	scanf( " %d", &nMatrices);
+	while( nMatrices > 0) {
+		FOR(m, 0, nMatrices) 
+			scanf(" %d %d", &matrix[m][0], &matrix[m][1]);
+
+		memset(memo, -1, sizeof memo);
+		optimalMult(0, nMatrices);
+		printf("Case %d: ", nCase++);
+		printMultiplySeq(0, nMatrices);
+		//printPos(nMatrices);
+		printf("\n");
+		scanf( " %d", &nMatrices);
+	}
+	fprintf(stderr, "Time taken: %.4f seconds\n", 
+			(float)(clock()-begin)/CLOCKS_PER_SEC);
+	return 0;
+}
