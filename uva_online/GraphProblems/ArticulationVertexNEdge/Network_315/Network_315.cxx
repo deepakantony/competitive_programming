036e4cf9034f034a1d83d3f47df36b7be157f9f6
Deepak <deepakantony@gmail.com>
2012-06-15 01:16:51 -0700

    getting runtime error; although the dfs implementation is complete

---
+++ uva_online/GraphProblems/ArticulationVertexNEdge/Network_315/Network_315.cxx
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+
+VI dfsNum, dfsLow, dfsParent, articulationVertex;
+VVI G;
+int dfsNumIndex;
+int dfsRoot, dfsRootChildren;
+
+void dfsArticulationVertCount(int curVertex)
+{
+	dfsNum[curVertex] = dfsLow[curVertex] = dfsNumIndex++;
+
+	for(int i = 0; i < G[curVertex].size(); i++) {
+		if(dfsNum[G[curVertex][i]] == 0) {
+			dfsParent[G[curVertex][i]] = curVertex;
+			if(curVertex == dfsRoot) dfsRootChildren++;
+			dfsArticulationVertCount(G[curVertex][i]);
+			if(dfsLow[G[curVertex][i]] >= dfsNum[curVertex])
+				articulationVertex[curVertex] = 1;
+			dfsLow[curVertex] = min(dfsLow[curVertex],
+									dfsLow[G[curVertex][i]]);
+		}
+		else if(dfsParent[G[curVertex][i]] != curVertex)
+			dfsLow[curVertex] = min(dfsLow[curVertex], 
+									dfsNum[G[curVertex][i]]);
+	}
+
+}
+
+
+int main(int argc, char *argv[])
+{
+	int N, curVertex;
+	char input[500];
+	while(scanf(" %d", &N), N != 0) {
+		G = VVI(N);
+		while(true)
+		{
+			scanf(" %[^\n]s", input);
+			sscanf(input, " %d", &curVertex);
+			if(curVertex == 0) break;
+			int len = strlen(input);
+			int connectedNode;
+			for(int i = 2; i < len; i+=2) {
+				sscanf(input+i, " %d", &connectedNode);
+				G[connectedNode-1].push_back(curVertex-1);
+				G[curVertex-1].push_back(connectedNode-1);
+			}
+		}
+
+/*		for(int i = 0; i < G.size(); i++) {
+			for(int j = 0; j < G[i].size(); j++) 
+				printf("%d ", G[i][j]);
+			printf("\n");
+			}*/
+
+		dfsNum = VI(N, 0);
+		dfsLow = VI(N);
+		dfsParent = VI(N);
+		dfsNumIndex = 1;
+		articulationVertex = VI(N, 0);
+		dfsRoot = 0;
+		dfsRootChildren = 0;
+		dfsArticulationVertCount(0);
+		articulationVertex[dfsRoot] = (dfsRootChildren > 1) ? 1: 0;
+		printf("%d\n", accumulate(articulationVertex.begin(),
+								  articulationVertex.end(), 0));
+	}
+
+	return 0;
+}
