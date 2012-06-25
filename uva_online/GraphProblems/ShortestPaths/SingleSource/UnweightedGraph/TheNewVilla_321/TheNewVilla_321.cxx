4108796dbb6287323b503e81fb3db026466a7ba2
Deepak <deepakantony@gmail.com>
2012-06-25 00:01:46 -0700

    started this; tougher than it appeared at first

---
+++ uva_online/GraphProblems/ShortestPaths/SingleSource/UnweightedGraph/TheNewVilla_321/TheNewVilla_321.cxx
@@ -0,0 +1,16 @@
+#include <vector>
+#include <queue>
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+
+void bfs
+
+int main(int argc, char *argv[])
+{
+	return 0;
+}
