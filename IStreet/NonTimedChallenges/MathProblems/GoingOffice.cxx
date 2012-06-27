7d74367ba614a6ad3036c445a746beb56782ae0f
Deepak Antony <deepakantony@gmail.com>
2012-06-26 19:48:51 -0700

    started going office

---
+++ IStreet/NonTimedChallenges/MathProblems/GoingOffice.cxx
@@ -0,0 +1,29 @@
+#include <queue>
+#include <vector>
+#include <cstdio>
+#include <utility>
+
+using namespace std;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef pair<int,int> PII;
+typedef pair<int,PII> PIPII;
+typedef vector<PII> VPII;
+typedef vector<VPII> VVPII;
+typedef priority_queue<PII> PQPII;
+typedef priority_queue<int> PQI;
+typedef priority_queue<PIPII> PQPIPII;
+
+#define REP(i, n) for(int i = 0; i < (n); ++i)
+#define FOR(i, st, n) for(int i = (st); i < (n); ++i)
+#define mp make_pair
+
+
+void dijkstraSSSP(const VVPII &G,  // adj list
+				  const int source,
+				  VI &distance,
+				  VI &path)
+{
+	PQ
+}
