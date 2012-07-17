c5e52d29bffb2464fa25431b1768b38677f571c6
Deepak Antony <deepakantony@gmail.com>
2012-07-16 18:33:06 -0700

    a bit more addition to the median code

--- IStreet/NonTimedChallenges/DataStructure/BST/Median.cxx
+++ IStreet/NonTimedChallenges/DataStructure/BST/Median.cxx
@@ -3,20 +3,49 @@
 using namespace std;
 
 template<typename T>
-class Node {
-public:
-	Node *next;
+struct Node {
+	Node(const T& _data) : left(NULL), right(NULL), parent(NULL), 
+						   data(_data), size(1) {}
+	Node<T> *left, *right, *parent;
 	T data;
 	int size;
+	int height;
 };
 
 template<typename T>
 class BST {
 public:
-	BST() {}
+	BST() : root(NULL) {}
+
+	int size() { root == NULL ? 0 : root.size(); }
+
+	void add(const T &data) {
+		if(root == NULL) root = new Node(data);
+		else add(root, data);
+	}
+
+	void remove(const T &data) {
+		if(root != NULL) {
+			Node<T> *todelete = findNodeWithValue(root, data);
+			remove(todelete);
+		}
+	}
+
+	float median() {
+		assert(root != NULL);
+		int mid = size/2;
+		if(root->size % 2 == 0)
+			return (float)(median(root, mid) + median(root, mid+1))/2.f;
+		else return median(root, mid);
+	}
+private:
+	void add(Node<T> *cur, const T &data) {
+	}
+
+	Node<T> *findNodeWithValue
 
 private:
-	Node<T> root;
+	Node<T> *root;
 };
 
 
