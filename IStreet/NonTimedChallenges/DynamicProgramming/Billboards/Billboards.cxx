2d67e277711ec05f02d418304fa0c8214c617320
Deepak <deepakantony@gmail.com>
2012-07-23 00:18:03 -0700

    solved with dp and heap

--- IStreet/NonTimedChallenges/DynamicProgramming/Billboards/Billboards.cxx
+++ IStreet/NonTimedChallenges/DynamicProgramming/Billboards/Billboards.cxx
@@ -1,14 +1,50 @@
+// I got the hint from here: http://ace.delos.com/TESTDATA/OPEN11.mowlawn.htm
+// calculate partial sum psum[i] = profit[0..i]
+// so the dp would be dp[i] = max[j=i-K to i](dp[j-1]+psum[i]-psum[j])
+// i.e. maximum total with profit[j] removed
+
 #include <iostream>
 #include <vector>
 #include <cstdio>
+#include <algorithm>
+#include <utility>
+#include <queue>
+
+#define FOR(i,st,nd) for(int i = (st); i < (nd); ++i)
+#define mp make_pair
 
 using namespace std;
+typedef long long int LL;
+typedef pair<LL, int> PLI;
 
 int main(int argc, char *argv[])
 {
-	vector<int> temp;
-	int *t = new int[100000*100000];
-	for(int i = 0; i < 100000*10000; ++i)
-		t[i] = 0;
-	cout << temp.max_size() << endl;
+	vector<LL> profits, partial;
+	priority_queue<PLI> maxLastK;
+
+	int N, K; scanf(" %d %d", &N, &K);
+	LL profit, sum = 0;
+
+	FOR(i, 0, N) {
+		scanf(" %lld", &profit);
+		profits.push_back(profit);
+		sum += profit;
+		partial.push_back(sum);
+	}
+
+	vector<LL> dp(N, 0);
+	// dp[i] = dp[j-1]+partial[i]-partial[j]; for j = i-K to i
+	maxLastK.push(mp(0, -1));
+	maxLastK.push(mp(-partial[0], 0));
+	dp[0] = partial[0];
+	FOR(i, 1, N) {
+		while(maxLastK.top().second < (i-K))
+			maxLastK.pop();
+		maxLastK.push(mp(dp[i-1]-partial[i], i));
+
+		dp[i] = partial[i] + maxLastK.top().first;
+		
+	}
+	printf("%lld\n", dp[N-1]);
+	return 0;
 }
