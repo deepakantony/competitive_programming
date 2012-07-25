f0614e05e795dbdcf03882beeb06865a982e6f61
Deepak <deepakantony@gmail.com>
2012-07-25 01:23:31 -0700

    solved with dp

--- IStreet/NonTimedChallenges/DynamicProgramming/LuckyNumber/LuckyNumber.cxx
+++ IStreet/NonTimedChallenges/DynamicProgramming/LuckyNumber/LuckyNumber.cxx
@@ -1,27 +1,17 @@
-#include <string>
-#include <algorithm>
-#include <vector>
-#include <numeric>
-#include <list>
-#include <iterator>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
 
-using namespace std;
+#define MAX_SUM_OF_SQUARES 1458 // (18 * 9 * 9) + 2
+#define MAX_SUM_OF_DIGITS 162 // (18 * 9) + 2
+#define TOTAL_DIGITS 18
+#define LARGEST_NUM 1000000000000000000ll // 10^18
 
-#define MAX_SUM_OF_SQUARES 1459 // (18 * 9 * 9) + 1
-#define MAX_SUM_OF_DIGITS 163 // (18 * 9) + 1
-
-typedef unsigned long long vlong;
+typedef long long vlong;
 
 int primes[MAX_SUM_OF_SQUARES+1];
 
 
-template<typename T>
-void printContainer(T container)
-{
-	cout << "( ";
-	copy(container.begin(), container.end(), ostream_iterator<int>(cout, ","));
-	cout << " )" << endl;
-}
 
 void initPrimes()
 {
@@ -42,21 +32,94 @@ void initPrimes()
 		while(index <= MAX_SUM_OF_SQUARES && primes[index] != -1)
 			index++;
 	}
-}	
-
-bool isPrime(int num)
-{
-	return (num <= MAX_SUM_OF_SQUARES && primes[num] == 1)?true:false;
 }
 
-void buildPrimeVector(vector<int> &primeVec)
-{
-	primeVec.clear();
-	primeVec.push_back(2);
-	for(int num = 3; num < MAX_SUM_OF_DIGITS; num+=2)
-	{
-		if(isPrime(num))
-			primeVec.push_back(num);
+
+// The solution is based on the solution given at 
+// http://stackoverflow.com/questions/9018614/algorithm-to-find-lucky-numbers
+
+vlong memo[TOTAL_DIGITS+2][MAX_SUM_OF_DIGITS+2][MAX_SUM_OF_SQUARES+2];
+int kstart[TOTAL_DIGITS+1][MAX_SUM_OF_DIGITS+1];
+int kend[TOTAL_DIGITS+1][MAX_SUM_OF_DIGITS+1];
+
+void preProcess() {
+	memset(memo, 0, sizeof memo);
+	memo[0][0][0] = 1;
+	
+	for(int i = 0; i < TOTAL_DIGITS; ++i) {
+		int jlimit = 9*i;
+		int klimit = 81*i;
+		for(int j = 0; j <= jlimit; ++j)
+			for(int k = 0;  k <= klimit; ++k) 
+				for(int l = 0; l < 10; ++l)
+					memo[i+1][j+l][k+l*l] += memo[i][j][k];
 	}
+
+	for(int i = 0; i <= TOTAL_DIGITS; ++i) 
+		for(int j = 0; j <= MAX_SUM_OF_DIGITS+1; ++j) {
+			for(int k = 0; k <= MAX_SUM_OF_SQUARES; ++k)
+				if(memo[i][j][k] != 0) { kstart[i][j] = k; break; }
+			for(int k = MAX_SUM_OF_SQUARES+2; k >= 0; --k)
+				if(memo[i][j][k] != 0) { kend[i][j] = k; break; }
+		}
 }
 
+vlong res[TOTAL_DIGITS+1][10][MAX_SUM_OF_DIGITS+2][MAX_SUM_OF_SQUARES+2];
+
+vlong luckyNumbersCount(vlong num) {
+	int nDigits = 0;
+	int digits[TOTAL_DIGITS];
+	while(num > 0) {
+		digits[nDigits++] = num%10;
+		num/=10;
+	}
+	int sum = 0, sumSq = 0; 
+	vlong result = 0;
+	for(int i = nDigits-1; i >= 0; --i) {
+		vlong stepRes = 0;
+		int jlimit = 9*i;
+
+		for(int l = 0; l < digits[i]; ++l)
+			if(res[i][l][sum][sumSq] != -1) 
+				stepRes += res[i][l][sum][sumSq];
+			else {
+				vlong step = 0;
+				int lsum = l+sum;
+				int llsum = l*l+sumSq;
+				for(int j = 0; j <= jlimit; ++j) 
+					if(primes[j+lsum])
+						for(int k = kstart[i][j]; k <= kend[i][j]; ++k)
+							if(primes[k+llsum]) {
+								step += memo[i][j][k];
+								stepRes += memo[i][j][k];
+							}
+				res[i][l][sum][sumSq] = step;
+			}
+		result += stepRes;
+		sum += digits[i];
+		sumSq += digits[i]*digits[i];
+	}
+	if(primes[sum] && primes[sumSq]) ++result;
+	return result;
+}
+
+vlong luckyNumbersCountRange(vlong start, vlong end) {
+	if(end == LARGEST_NUM) end--;
+	return luckyNumbersCount(end) - luckyNumbersCount(start-1);
+}
+
+int main(int argc, char *argv[]) {
+	clock_t start = clock();
+	int T; scanf(" %d", &T);
+	vlong A, B;
+	initPrimes();
+	preProcess();
+	memset(res, -1, sizeof res);
+	while(T--) {
+		scanf(" %lld %lld", &A, &B);
+		printf("%lld\n", luckyNumbersCountRange(A,B));
+	}
+	fprintf(stderr, "Time taken: %.3fs\n", (clock()-start)/(float)CLOCKS_PER_SEC);
+
+	return 0;
+}
