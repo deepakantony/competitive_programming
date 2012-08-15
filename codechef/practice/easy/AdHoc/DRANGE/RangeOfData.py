694e3cb27dc5e092c1bd29b05cd78d01d4c29d66
Deepak Antony <deepakantony@gmail.com>
2012-08-15 14:07:26 -0700

    although the solution is O(m*log(m)), python is too slow for the judge

--- codechef/practice/easy/AdHoc/DRANGE/RangeOfData.py
+++ codechef/practice/easy/AdHoc/DRANGE/RangeOfData.py
@@ -1,21 +1,40 @@
 import os
+import math
 
 # read input and implement a O(M*log(M))
 def main():
     T = int(raw_input())
     for test in xrange(T):
         N, M = tuple([ int(x) for x in raw_input().split()])
-        mUpdates = [(1, 1), (N, 0)]
+        mUpdates = [(N, 0)]
         # just store the range updates since the changes happend only at the 
         # ends. 
         for update in xrange(M):
             w, x, y, z = tuple([ int(a) for a in raw_input().split()])
             if w == 2: z = -z
-            mUpdates.append(tuple(x, z))
-            mUpdates.append(tuple(y, -z))
 
-        sorted(mUpdates)
-        
+            mUpdates.append(tuple([x-1, z]))
+            mUpdates.append(tuple([y, -z]))
+
+        mUpdates.sort()
+
+        i = 0 # curIndex
+        a_i = 1
+        d_i_1 = 0
+        maxSoFar = -float('inf')
+        minSoFar = float('inf')
+        for cur in mUpdates:
+            nexti = cur[0]
+            if nexti != i:
+                maxSoFar = max(maxSoFar, a_i+nexti-i-1)
+                minSoFar = min(minSoFar, a_i)
+                #print maxSoFar, minSoFar
+                a_i = a_i+nexti-i
+            a_i += cur[1]
+            i = nexti
+
+        print maxSoFar-minSoFar
 
 if __name__ == "__main__":
     main()
+
