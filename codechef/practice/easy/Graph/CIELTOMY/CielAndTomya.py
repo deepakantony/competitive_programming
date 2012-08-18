0d8bef874f5eb160fced43f5c83319ef17d8b24e
Deepak Antony <deepakantony@gmail.com>
2012-08-17 19:13:39 -0700

    started with dijkstras implementation

---
+++ codechef/practice/easy/Graph/CIELTOMY/CielAndTomya.py
@@ -0,0 +1,12 @@
+import heapq
+
+# 
+def main():
+    T = int(raw_input())
+    for test in xrange(T):
+        N, M = tuple( map(int, raw_input().split()) )
+        for edge in xrange(M):
+            Ai, Bi, Ci = tuple( map(int, raw_input().split()) )
+
+if __name__ == "__main__":
+    main()
