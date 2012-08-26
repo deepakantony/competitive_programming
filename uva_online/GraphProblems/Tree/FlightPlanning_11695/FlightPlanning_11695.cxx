fc264ba553ed34904cf3b7f36ba6fb628a1fa1ff
Deepak <deepakantony@gmail.com>
2012-08-26 04:04:05 -0700

    started the problem

---
+++ uva_online/GraphProblems/Tree/FlightPlanning_11695/FlightPlanning_11695.cxx
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+#define REP(i, n) for(int i = 0; i < (n); ++i)
+#define FOR(i, st, nd) for(int i = (st); i < (nd); ++i)
+#define SET(a, val) memset(a, val, sizeof(a))
+#define pb push_back
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+
+int main(int argc, char *argv[]) {
+
+	return 0;
+}
