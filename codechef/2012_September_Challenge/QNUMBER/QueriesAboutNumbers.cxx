f2d577883d67a022922b6f7324492828a567cdb3
Deepak <deepakantony@gmail.com>
2012-09-09 03:54:44 -0700

    initial version

---
+++ codechef/2012_September_Challenge/QNUMBER/QueriesAboutNumbers.cxx
@@ -0,0 +1,39 @@
+/*
+  Problem:
+  Link:
+  Author: deebee
+  Solution: 
+*/
+
+#include <cstdio>
+#include <cstring>
+#include <cassert>
+#include <ctime>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long UL;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<LL> VL;
+typedef vector<VL> VVL;
+
+#define REP(i, n) for(int i = 0; i < (n); ++i)
+#define FOR(i, st, nd) for(int i = (st); i < (nd); ++i)
+#define SET(a, val) memset(a, val, sizeof(a))
+
+int main(int argc, char *argv[]) {
+#ifndef ONLINE_JUDGE
+	clock_t start = clock();
+#endif 
+	// solve something here
+
+#ifndef ONLINE_JUDGE
+	fprintf(stderr, "Time taken : %.5f seconds.\n",
+			((float)clock()-start)/CLOCKS_PER_SEC);
+#endif
+	return 0;
+}
