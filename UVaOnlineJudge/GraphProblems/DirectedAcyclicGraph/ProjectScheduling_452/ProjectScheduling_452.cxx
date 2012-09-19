a5857abdee01620b81bb6fef2696de6c309da535
Deepak <deepakantony@gmail.com>
2012-09-18 23:27:03 -0700

    needed a DP longest path algo for DAG

--- UVaOnlineJudge/GraphProblems/DirectedAcyclicGraph/ProjectScheduling_452/ProjectScheduling_452.cxx
+++ UVaOnlineJudge/GraphProblems/DirectedAcyclicGraph/ProjectScheduling_452/ProjectScheduling_452.cxx
@@ -27,12 +27,28 @@ typedef vector<VL> VVL;
 
 VVI G;
 VI nodeTime;
+VI memo;
+
+// DP for longest path
+// lp(i) = max(time(i) + lp(j)) for every j adjacent to vertex i
+// lp(i) = time(i) when there are no adjacent vertex to i
+int lp(int u) {
+	if(memo[u] != -1) return memo[u];
+	if(G[u].size() == 0) return memo[u] = nodeTime[u];
+	int res = 0;
+	REP(i, G[u].size()) 
+		res = max(res, lp(G[u][i]));
+	return memo[u] = res + nodeTime[u];
+}
 
 int longestPath() {
-	// find topological sort
-
 	// find the longest path
-	return 0;
+	memo.assign(nodeTime.size(), -1);
+	int res = 0;
+	REP(i, nodeTime.size()) 
+		if(memo[i] == -1) res = max(res,lp(i));
+
+	return res;
 }
 
 void readInput() {
@@ -40,19 +56,23 @@ void readInput() {
 	char curN; int t; char nodes[50];
 	G.assign(26, VI());
 	nodeTime.assign(26, 0);
-	while( scanf("%[\n]s", inp) != EOF && inp[0] != 0 && inp[0] != '\n') {
-		sscanf(inp, "%c %d %s",&curN, &t, nodes);
+	while( fgets(inp, 100, stdin) != NULL && inp[0] != 0 && inp[0] != '\n') {
+		int count = sscanf(inp, "%c %d %s",&curN, &t, nodes);
 		int v = curN - 'A';
 		nodeTime[v] = t;
-		for(int i = 0; nodes[i]; ++i)
-			G[nodes[i]-'A'].push_back(v);
+
+		if(count == 3) 
+			for(int i = 0; nodes[i]; ++i)
+				G[nodes[i]-'A'].push_back(v);
 	}
+	//REP(i, nodeTime.size()) printf("%d ", nodeTime[i]); printf("\n");
 }
 
 void solveProjectScheduling() {
-	int T; scanf("%d", &T);
+	int T; scanf("%d\n", &T);
 	
 	while(T--) {
+		scanf("\n");
 		readInput();
 		printf("%d\n", longestPath());
 		if(T!=0) printf("\n");
