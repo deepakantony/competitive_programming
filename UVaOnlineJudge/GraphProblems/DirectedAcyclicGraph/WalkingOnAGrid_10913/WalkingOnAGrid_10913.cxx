7a9b57ac4623d004b0cbcf9365337ef1f8536848
Deepak <deepakantony@gmail.com>
2012-09-28 00:32:45 -0700

    applied a wrong DP function; need a DP which handles incoming direction; added input cases from forum

--- UVaOnlineJudge/GraphProblems/DirectedAcyclicGraph/WalkingOnAGrid_10913/WalkingOnAGrid_10913.cxx
+++ UVaOnlineJudge/GraphProblems/DirectedAcyclicGraph/WalkingOnAGrid_10913/WalkingOnAGrid_10913.cxx
@@ -33,10 +33,58 @@ typedef vector<VPII> VVPII;
 #define SET(a, val) memset(a, val, sizeof(a))
 #define mp make_pair
 
-#define INF numeric_limits<int>::max()
+#define INF 1000000000000
+
+int N, K;
+int grid[75][75];
+LL memo[75][75][6];
+int visited[75][75];
+int level;
+
+LL maxPath(int row, int col, int kLeft) {
+	if(kLeft < 0) return -INF;
+
+	REP(i, level) printf("\t");
+	printf("%d %d %d\n", row, col, kLeft);	
+	++level;
+	visited[row][col] = 1;
+	LL &res = memo[row][col][kLeft];
+	if(res == -(INF+1)) {
+		if(kLeft == 0 && grid[row][col] == -1) res = -INF;
+		else if(row == N-1 && col == N-1) res = grid[row][col];
+		else {
+			res = grid[row][col];
+			if(res < 0) --kLeft;
+			LL maximumRes = -INF;
+			if(col > 0 && visited[row][col-1] == 0) 
+				maximumRes = max(maximumRes, maxPath(row, col-1, kLeft));
+			if(col < N-1 && visited[row][col+1] == 0)
+				maximumRes = max(maximumRes, maxPath(row, col+1, kLeft));
+			if(row < N-1 && visited[row+1][col] == 0)
+				maximumRes = max(maximumRes, maxPath(row+1, col, kLeft));
+			if(res < 0) ++kLeft;
+			if(maximumRes <= -INF) res = -INF;
+			else res += maximumRes;
+		}
+	}
+	visited[row][col] = 0;
+	--level;
+	REP(i, level) printf("\t");
+	printf("%d %d %d %lld\n", row, col, kLeft, res);
+
+	return res;
+}
+
 
 void solveWalkingOnAGrid() {
-
+	int test = 1;
+	while(scanf("%d%d", &N, &K) != EOF && N > 0) {
+		REP(i, N) REP(j, N) scanf("%d", &grid[i][j]);
+		REP(i, N) REP(j, N) REP(k, K+1) memo[i][j][k] = -INF-1;
+		LL res = maxPath(0, 0, K);
+		if(res <= -INF) printf("Case %d: impossible\n", test++);
+		else printf("Case %d: %lld\n", test++, res);
+	}
 }
 
 int main(int argc, char *argv[]) {
