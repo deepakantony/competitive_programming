4d5b520de5a17932e2ccb558633beebbe4963fb1
Deepak <deepakantony@gmail.com>
2012-10-07 01:54:06 -0700

    first version of the template for solving problem in python

---
+++ Templates/TemplateMainPy2x.py
@@ -0,0 +1,31 @@
+# ******************************************************************************
+# Problem: Template Python file.
+# Link:
+# Author: Deepak Antony - "deebee" "drunkbeast" "dAbeAst"
+# Solution: 
+# ******************************************************************************
+
+# Uncomment this line if the judge supports psyco
+# import psyco
+# psyco.full()
+
+import time
+import sys
+
+def timeElapsed(fn):
+    """ A time elapsed decorator """
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        res = fn(*args, **kwargs)
+        sys.stderr.write("Time Elapsed: %f seconds\n"%(time.time()-start))
+        return res
+    return wrapper
+
+@timeElapsed
+def main():
+    """ Main controller """
+    # solve a problem here
+    pass
+
+if __name__ == "__main__":
+    main()
