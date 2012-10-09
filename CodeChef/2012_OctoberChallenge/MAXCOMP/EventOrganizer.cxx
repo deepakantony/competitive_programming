95766b6d8ba874e06b20875a0cf279f97199b0a0
Deepak Antony <deepakantony@gmail.com>
2012-10-09 15:12:00 -0700

    changed the code to use arrays instead of vectors

--- CodeChef/2012_OctoberChallenge/MAXCOMP/EventOrganizer.cxx
+++ CodeChef/2012_OctoberChallenge/MAXCOMP/EventOrganizer.cxx
@@ -33,24 +33,17 @@ typedef vector<VPII> VVPII;
 #define mp make_pair
 
 #define INF numeric_limits<int>::max()
-#define GRAPH_SIZE 50
+#define GRAPH_SIZE 49
 
-VVPII G;
-
-void initGraph() {
-	G.assign(GRAPH_SIZE, VPII());
-	REP(i, G.size()) FOR(j, i+1, G.size())
-		G[i].push_back(PII(j, 0));
-}
-
-VL memo;
+int G[50][50];
+LL memo[50];
 
 LL longestPath(int curNode) {
 	if(memo[curNode] == -1) {
 		memo[curNode] = 0;
-		REP(i, G[curNode].size()) {
-			memo[curNode] = max(memo[curNode], G[curNode][i].second + 
-						  longestPath(G[curNode][i].first));
+		FOR(i, curNode+1, GRAPH_SIZE) {
+			memo[curNode] = max(memo[curNode], G[curNode][i] +
+						  longestPath(i));
 		}
 	}
 	return memo[curNode];
@@ -60,14 +53,14 @@ void solveEventOrganizer() {
 	int T; scanf("%d", &T);
 	int N, u, v, w;
 	while(T--) {
-		initGraph();
+		SET(G, 0);
 		scanf("%d", &N);
 		while(N--) {
-			scanf("%d%d%d", &u, &v, &w);
-			G[u].push_back(PII(v, w));
+			scanf("%d %d %d", &u, &v, &w);
+			G[u][v] = max(G[u][v], w);
 		}
 
-		memo.assign(G.size(), -1);
+		SET(memo, -1);
 		//LL lp = 0;
 		//REP(i, G.size()) if(memo[i] == -1) lp = max(lp, longestPath(i));
 		printf("%lld\n", longestPath(0));
