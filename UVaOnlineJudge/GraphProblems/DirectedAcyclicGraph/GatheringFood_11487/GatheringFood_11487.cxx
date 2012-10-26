9d33d323359c012ac780b01affd750ac338828ef
Deepak <deepakantony@gmail.com>
2012-10-26 00:11:41 -0700

    solved; DP with DAG

--- UVaOnlineJudge/GraphProblems/DirectedAcyclicGraph/GatheringFood_11487/GatheringFood_11487.cxx
+++ UVaOnlineJudge/GraphProblems/DirectedAcyclicGraph/GatheringFood_11487/GatheringFood_11487.cxx
@@ -15,6 +15,7 @@
 #include <algorithm>
 #include <utility>
 #include <limits>
+#include <queue>
 
 using namespace std;
 
@@ -35,9 +36,102 @@ typedef vector<VPII> VVPII;
 #define mp make_pair
 
 #define INF numeric_limits<int>::max()
+#define MOD 20437
+
+int N;
+char forest[10][11];
+char lastChar;
+int rowStart, colStart;
+int shortestPath;
+int arrs[] = {-1, 1, 0, 0};
+int sees[] = {0, 0, -1, 1};
+
+void parseForest() {
+	lastChar = 'A';
+	REP(i, N) REP(j, N)
+		if(isalpha(forest[i][j])) lastChar = max(forest[i][j], lastChar);
+}
+
+void findLocation(char searchChar) {
+	REP(i, N) REP(j, N)
+		if(forest[i][j] == searchChar) rowStart = i, colStart = j;
+}
+
+bool isValid(int row, int col) {
+	return row < N && col < N && row >= 0 && col >= 0 && 
+		forest[row][col] != '#' && 
+		(forest[row][col] == '.' || 
+		 forest[row][col] <= forest[rowStart][colStart]+1);
+}
+
+LL bfsShortestPath(char from, char to) {
+	shortestPath = INF;
+	
+	VVL paths(N, VL(N, 0));
+	paths[rowStart][colStart] = 1;
+	VVI dist(N, VI(N, INF));
+	dist[rowStart][colStart] = 0;
+	queue<pair<int ,PII> > Q;
+	Q.push(mp(0, mp(rowStart, colStart)));
+	pair<int, PII> curNode;
+	int curRow, curCol, curLength, r, c;
+	while(!Q.empty() && Q.front().first < shortestPath) {
+		curNode = Q.front(); Q.pop();
+		curLength = curNode.first;
+		curRow = curNode.second.first;
+		curCol = curNode.second.second;
+		//printf("(%d %d) %d %lld\n", curRow, curCol, curLength, paths[curRow][curCol]);
+		for(int i = 0; i < 4; ++i) {
+			r = curRow+arrs[i];
+			c = curCol+sees[i];
+			if(isValid(r, c)) 
+				if(dist[r][c] == INF) {
+					dist[r][c] = curLength+1;
+					paths[r][c] += paths[curRow][curCol];
+					Q.push(mp(curLength+1, mp(r, c)));
+					if(forest[r][c] == to) 
+						shortestPath = curLength + 1;
+				}
+				else if(dist[r][c] == curLength+1)
+					paths[r][c] += paths[curRow][curCol];
+		}
+	}
+	
+	REP(i, N) REP(j, N) if(forest[i][j] == to) 
+	{ rowStart = i, colStart = j; }
+
+	return paths[rowStart][colStart];
+}
 
 void solveGatheringFood() {
+	int test = 1;
+	int nPaths;
+	LL numOfDistinctPaths;
+	char from;
+	int sp;
+	while(scanf("%d",&N) != EOF && N != 0) {
+		REP(i, N) scanf("%s",forest[i]);
 
+		// identify the last character.
+		parseForest();
+		findLocation('A');
+		numOfDistinctPaths = 1;
+		sp = 0;
+		for(from = 'A'; from != lastChar; ++from) {
+
+			nPaths = bfsShortestPath(from, from+1);
+			sp += shortestPath;
+			if(nPaths == 0) break;
+
+			numOfDistinctPaths = (numOfDistinctPaths*nPaths)%MOD;
+		}
+
+		printf("Case %d: ", test++);
+		if(from == lastChar)
+			printf("%d %lld\n", sp, numOfDistinctPaths);
+		else
+			printf("Impossible\n");
+	}
 }
 
 int main(int argc, char *argv[]) {
