3d95339b7aefad8db839b5cf76635cf0f463e50b
Deepak <deepakantony@gmail.com>
2013-05-05 05:09:42 -0700

    matchsticks problem - initial version

---
+++ code_chef/2013_may_challenge/matchsticks/matchsticks.cpp
@@ -0,0 +1,48 @@
+/*
+  Problem: matchsticks
+  Link: http://ww2.codechef.com/MAY13/problems/MSTICK
+  Author: Deepak Antony - "deebee" "drunkbeast" "dAbeAst"
+  Solution: segment tree problem
+*/
+
+#include <cstdio>
+#include <cstring>
+#include <cassert>
+#include <ctime>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <limits>
+
+using namespace std;
+
+typedef unsigned long long UL;
+typedef long long LL;
+typedef pair<int,int> PII;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<LL> VL;
+typedef vector<VL> VVL;
+typedef vector<PII> VPII;
+typedef vector<VPII> VVPII;
+
+#define REP(i, n) for(int i = 0; i < (n); ++i)
+#define FOR(i, st, nd) for(int i = (st); i < (nd); ++i)
+#define SET(a, val) memset(a, val, sizeof(a))
+#define mp make_pair
+
+#define INF numeric_limits<int>::max()
+
+int main(int argc, char *argv[]) {
+#ifndef ONLINE_JUDGE
+	clock_t start = clock();
+#endif 
+	// solve something here
+
+#ifndef ONLINE_JUDGE
+	fprintf(stderr, "Time taken : %.5f seconds.\n",
+			((float)clock()-start)/CLOCKS_PER_SEC);
+#endif
+	return 0;
+}
