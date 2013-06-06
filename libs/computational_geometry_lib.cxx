31f513c4fbac3a08e67e72d9183314a51228f68b
Deepak <deepakantony@gmail.com>
2013-06-06 00:33:03 -0700

    started the class line_t

--- libs/computational_geometry_lib.cxx
+++ libs/computational_geometry_lib.cxx
@@ -6,6 +6,15 @@ return_type hypotenuse(const length_type side_a, const length_type side_b)
 	return return_type( sqrt( side_a * side_a + side_b * side_b ) );
 }
 
+// Forward declarations
+template< typename coordinate_type > class point_t;
+class line_t;
+class vector_t;
+class line_segment_t;
+class circle_t;
+class quadrilateral_t;
+class polygon_t;
+
 //
 // Point class
 template<typename coordinate_type>
@@ -31,37 +40,57 @@ public:
 	}
 
 	bool operator< (const point_t<coordinate_type> &other_point) const {
-		if( x_ == other_point.x_ ) return y_ < other_point.y_;
+		if( equals( x_, other_point.x_ ) ) return y_ < other_point.y_;
 		else return x_ < other_point.x_;
 	}
 
 	bool operator== (const point_t<coordinate_type> &other_point) const {
-		return (x_ == other_point.x_) && (y_ == other_point.y_);
+		return ( equals( x_, other_point.x_ ) ) && ( equals(y_, other_point.y_ ) );
 	}
+
+	coordinate_type x() { return x_; }
+	coordinate_type y() { return y_; }
 
 private:
 	coordinate_type x_, y_;
 };
 
-template<> bool point_t<double>::operator< (const point_t<double> &other_point) const
+//
+// Line class
+class line_t
 {
-	if( FLOAT_EQUALS( x_, other_point.x_ ) )
-		return y_ < other_point.y_;
-	else
-		return x_ < other_point.x_;
-}
-template<> bool point_t<float>::operator< (const point_t<float> &other_point) const
-{
-	if( FLOAT_EQUALS( x_, other_point.x_ ) )
-		return y_ < other_point.y_;
-	else
-		return x_ < other_point.x_;
-}
-template<> bool point_t<double>::operator== (const point_t<double> &other_point) const
-{
-	return ( FLOAT_EQUALS( x_, other_point.x_ ) ) && ( FLOAT_EQUALS( y_, other_point.y_ ) );
-}
-template<> bool point_t<float>::operator== (const point_t<float> &other_point) const
-{
-	return ( FLOAT_EQUALS( x_, other_point.x_ ) ) && ( FLOAT_EQUALS( y_, other_point.y_ ) );
-}
+public:
+	line_t() : a_(0.0), b_(0.0), c_(0.0) {}
+
+	// direct coefficients
+	line_t( double a, double b, double c ) : a_(a), b_(b), c_(c) {}
+
+	// two point formula
+	template<typename coordinate_type>
+	line_t( point_t<coordinate_type> point_1,
+			point_t<coordinate_type> point_2 
+		) {
+		a_ = (double)( point_2.y() - point_1.y() );
+		c_ = (double)( point_2.x() * point_1.y() - point_1.x() * point_2.y() );
+		if( equals( point_1.x(), point_2.x() ) ) 
+			b_ = 0.0;
+		else
+		{
+			a_ = -a_ / (double)( point_2.x() - point_2.y() );
+			b_ = 1.0;
+			c_ = -c_ / (double)( point_2.x() - point_2.y() );
+		}
+	}
+
+	// slope intercept formula
+	line_t( double m, double c ) : a_(-m), b_(1.0), c_(-c) {}
+
+	// is parallel with another line
+	bool is_parallel( const line_t &other ) {
+		return ( equals( a_, other.a_ ) && equals( b_, other.b_ ) );
+	}
+
+private:
+	double a_, b_, c_;
+};
+
