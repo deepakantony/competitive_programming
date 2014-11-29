b277af52357fa950fcf8dc8a95885b37acd2a27e
Deepak <deepakantony@gmail.com>
2014-11-29 02:49:31 -0800

    started solving problems at hacker rank

---
+++ hacker_rank/algorithms/warmup/find_digits/find_digits.cpp
@@ -0,0 +1,73 @@
+/*
+  Problem: Find digits
+  Link: https://www.hackerrank.com/challenges/find-digits
+  Author: Deepak Antony - "deebee" "drunkbeast" "dAbeAst"
+  Solution: iterate through digits and see if it divide the N
+*/
+
+#include <cstdio>
+#include <cstring>
+#include <cassert>
+#include <ctime>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <limits>
+
+using namespace std;
+
+typedef unsigned long long UL;
+typedef long long LL;
+typedef pair<int,int> PII;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<LL> VL;
+typedef vector<VL> VVL;
+typedef vector<PII> VPII;
+typedef vector<VPII> VVPII;
+
+#define REP(i, n) for(int i = 0; i < (n); ++i)
+#define FOR(i, st, nd) for(int i = (st); i < (nd); ++i)
+#define SET(a, val) memset(a, val, sizeof(a))
+#define mp make_pair
+
+#define INF numeric_limits<int>::max()
+
+void solve_find_digits()
+{
+	int T, N, d, tempN, count;
+	scanf("%d", &T);
+
+	while(T--)
+	{
+		scanf("%d", &N);
+		tempN = N;
+
+		count = 0;
+		while( tempN > 0 )
+		{
+			d = tempN % 10;
+			if( d != 0 && (N%d) == 0 )
+				count++;
+
+			tempN /= 10;
+		}
+
+		printf( "%d\n", count);
+	}
+}
+
+int main(int argc, char *argv[]) {
+#ifndef ONLINE_JUDGE
+	clock_t start = clock();
+#endif 
+	// solve something here
+	solve_find_digits();
+
+#ifndef ONLINE_JUDGE
+	fprintf(stderr, "Time taken : %.5f seconds.\n",
+			((float)clock()-start)/CLOCKS_PER_SEC);
+#endif
+	return 0;
+}
