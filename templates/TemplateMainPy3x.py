23a7351bd535d2e394d88334b81df0f48f96b453
Deepak Antony <deepakantony@gmail.com>
2014-12-15 23:25:50 -0800

    Created template for python3

---
+++ templates/TemplateMainPy3x.py
@@ -0,0 +1,27 @@
+# **************************************************************************
+# Problem: Template Python file.
+# Link:
+# Author: Deepak Antony - "deebee" "drunkbeast" "dAbeAst"
+# Solution: 
+# **************************************************************************
+
+import time
+import sys
+
+def timeElapsed(fn):
+    """ A time elapsed decorator """
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        res = fn(*args, **kwargs)
+        sys.stderr.write("Time Elapsed: %f seconds\n"%(time.time()-start))
+        return res
+    return wrapper
+
+@timeElapsed
+def main():
+    """ Main controller """
+    # solve a problem here
+    pass
+
+if __name__ == "__main__":
+    main()
