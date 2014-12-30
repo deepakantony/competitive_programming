87242c285117a00a08763726e1eb86907f720752
Deepak <deepakantony@gmail.com>
2014-12-29 23:45:14 -0800

    solved verify anagrams

---
+++ check_io/scientific_expedition/verify_anagrams.py
@@ -0,0 +1,11 @@
+def verify_anagrams(first_word, second_word):
+    cleanup = lambda s: ''.join(sorted(s.replace(" ", "").lower()))
+    return cleanup(first_word) == cleanup(second_word)
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert isinstance(verify_anagrams("a", 'z'), bool), "Boolean!"
+    assert verify_anagrams("Programming", "Gram Ring Mop") == True, "Gram of code"
+    assert verify_anagrams("Hello", "Ole Oh") == False, "Hello! Ole Oh!"
+    assert verify_anagrams("Kyoto", "Tokyo") == True, "The global warming crisis of 3002"
+
