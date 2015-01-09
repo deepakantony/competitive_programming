75dd30e703f2fb3415f0c5dea2bdaf46e9ea9da4
Deepak <deepakantony@gmail.com>
2015-01-09 00:53:16 -0800

    blah i hate this problem sofar!

--- check_io/oreilly/text_holes_golf.py
+++ check_io/oreilly/text_holes_golf.py
@@ -1,39 +1,38 @@
 n=[(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
-e=enumerate
-def golf(t,a=0):
- for r,l in e(t):
-  for c,v in e(l):
-   if(v==' 'and all(0<=(r+i)<len(t)and 0<=(c+j)<len(t[r+i])and t[r+i][c+j]!=' 'for i,j in n)):
-    print(v,r,c)
-    a+=1
+def golf(t,a=0,r=1):
+ while r+1<len(t):
+  c=1
+  while c+1<len(t[r]):a+=t[r][c]==' '*all(c+j<len(t[r+i])and t[r+i][c+j]!=' 'for i,j in n);c+=1
+  r+=1
  return a
 
-assert golf(["How are you doing?",
-             "I'm fine. OK. Test",
-             "Lorem Ipsum?",
-             "Of course!!!",
-             "1234567890",
-             "0        0",
-             "1234567890",
-             "Fine! good buy!"]) == 3, "Test 1"
-print()
-assert golf(["Lorem ipsum dolor",
-             "sit amet,",
-             "consectetuer",
-             "adipiscing elit.",
-             "Aenean commodo",
-             "ligula eget dolor.",
-             "Aenean massa. Cum",
-             "sociis natoque",
-             "penatibus et magnis",
-             "dis parturient",
-             "montes, nascetur",
-             "ridiculus mus. Donec",
-             "quam felis,",
-             "ultricies nec,",
-             "pellentesque eu,",
-             "pretium quis, sem.",
-             "Nulla consequat",
-             "massa quis enim.",
-             "Donec pede justo,",
-             "fringilla vel,"]) == 11, "Test 2"
+if __name__ == "__main__":
+ assert golf(["How are you doing?",
+              "I'm fine. OK. Test",
+              "Lorem Ipsum?",
+              "Of course!!!",
+              "1234567890",
+              "0        0",
+              "1234567890",
+              "Fine! good buy!"]) == 3, "Test 1"
+ print()
+ assert golf(["Lorem ipsum dolor",
+              "sit amet,",
+              "consectetuer",
+              "adipiscing elit.",
+              "Aenean commodo",
+              "ligula eget dolor.",
+              "Aenean massa. Cum",
+              "sociis natoque",
+              "penatibus et magnis",
+              "dis parturient",
+              "montes, nascetur",
+              "ridiculus mus. Donec",
+              "quam felis,",
+              "ultricies nec,",
+              "pellentesque eu,",
+              "pretium quis, sem.",
+              "Nulla consequat",
+              "massa quis enim.",
+              "Donec pede justo,",
+              "fringilla vel,"]) == 11, "Test 2"
