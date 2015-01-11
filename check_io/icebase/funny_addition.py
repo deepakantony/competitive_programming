67e904b0a4ac7dbd2ce052dbf386af1fa4dd234c
Deepak <deepakantony@gmail.com>
2015-01-11 01:27:12 -0800

    funny

---
+++ check_io/icebase/funny_addition.py
@@ -0,0 +1,10 @@
+def checkio(data):
+    """The sum of two integer elements"""
+    return sum(data)
+
+# or checkio=sum
+    
+if __name__ == '__main__':
+    assert checkio([5, 5]) == 10, 'First'
+    assert checkio([7, 1]) == 8, 'Second'
+    print('All ok')
