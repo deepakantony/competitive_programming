eed8f1f1fdfa048cb5f4b5381bb7f66f06950325
Deepak Antony <deepakantony@a-08e-dantony-0.dts.local>
2015-01-13 19:48:32 -0800

    8 queens problem solved with backtracking

--- check_io/icebase/place_queens.py
+++ check_io/icebase/place_queens.py
@@ -1,21 +1,36 @@
+COLS = "abcdefgh"
+ROWS = "12345678"
+
+THREATS = {c + r: set(
+    [c + ROWS[k] for k in range(8)] +
+    [COLS[k] + r for k in range(8)] +
+    [COLS[k] + ROWS[i - j + k] for k in range(8) if 0 <= i - j + k < 8] +
+    [COLS[k] + ROWS[- k + i + j] for k in range(8) if 0 <= - k + i + j < 8])
+           for i, r in enumerate(ROWS) for j, c in enumerate(COLS)}
+
 def invalid_queens(placed):
-    
-    return True
+    for p in placed:
+        if any( pl in THREATS[p] for pl in placed if pl != p ):
+            return True
+    return False
 
 def get_first_row_not_occupied(placed):
-    return ord(min(placed)[0])-ord('a')+1 if placed else 1
+    occupied_rows = [x[1] for x in placed]
+    for r in ROWS:
+        if r not in occupied_rows: return r
+    return str(len(ROWS)+1) # all rows are occupied
 
 def place_queens(placed):
     res = set()
     if invalid_queens(placed): return res
 
     row = get_first_row_not_occupied(placed)
-    if row >= 8:
+    if int(row) > len(ROWS):
         res = placed
     else:
-        for col in range(1,9):
-            res = place_queens(placed + {chr(ord('a')+row-1)+str(col)})
-            if res:break;
+        for col in COLS:
+            res = place_queens(placed.union({col+row}))
+            if res: break;
 
     return res
 
@@ -23,15 +38,6 @@ def place_queens(placed):
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
     from itertools import combinations
-    COLS = "abcdefgh"
-    ROWS = "12345678"
-
-    THREATS = {c + r: set(
-        [c + ROWS[k] for k in range(8)] +
-        [COLS[k] + r for k in range(8)] +
-        [COLS[k] + ROWS[i - j + k] for k in range(8) if 0 <= i - j + k < 8] +
-        [COLS[k] + ROWS[- k + i + j] for k in range(8) if 0 <= - k + i + j < 8])
-               for i, r in enumerate(ROWS) for j, c in enumerate(COLS)}
 
     def check_coordinate(coor):
         c, r = coor
@@ -39,10 +45,13 @@ if __name__ == '__main__':
 
     def checker(func, placed, is_possible):
         user_set = func(placed.copy())
+        print ( placed, "=>", user_set )
+        
         if not all(isinstance(c, str) and len(c) == 2 and check_coordinate(c) for c in user_set):
             print("Wrong Coordinates")
             return False
         threats = []
+
         for f, s in combinations(user_set.union(placed), 2):
             if s in THREATS[f]:
                 threats.append([f, s])
@@ -52,6 +61,9 @@ if __name__ == '__main__':
                 return False
             else:
                 return True
+        if is_possible and len(user_set) != 8:
+            print("You've not placed all 8 queens.")
+            return False
         if not all(p in user_set for p in placed):
             print("You forgot about placed queens.")
             return False
@@ -62,4 +74,7 @@ if __name__ == '__main__':
 
     assert checker(place_queens, {"b2", "c4", "d6", "e8"}, True), "1st Example"
     assert checker(place_queens, {"b2", "c4", "d6", "e8", "a7", "g5"}, False), "2nd Example"
+    assert checker(place_queens, {"b2", "f7"}, True), "bug finder2"
+    assert checker(place_queens, {"b3", "d4", "f5"}, False), "bug finder1"
+    
 
