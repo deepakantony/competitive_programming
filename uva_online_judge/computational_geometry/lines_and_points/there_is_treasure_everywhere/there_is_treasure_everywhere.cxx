498364a8b7d7d89d12dc032998284bb80dab2e27
Deepak <deepakantony@gmail.com>
2015-10-30 01:16:55 -0700

    started this parsing problem

---
+++ uva_online_judge/computational_geometry/lines_and_points/there_is_treasure_everywhere/there_is_treasure_everywhere.cxx
@@ -0,0 +1,92 @@
+/*
+  Problem: Template CPP main file.
+  Link: https://uva.onlinejudge.org/external/5/587.html
+  Author: Deepak Antony - "deebee" "drunkbeast" "dAbeAst"
+  Solution: find the end coordinate by walking the instructions and then calculate distance
+*/
+
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <cassert>
+#include <ctime>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <limits>
+#include <iostream>
+#include <iomanip>
+#include <map>
+#include <sstream>
+
+using namespace std;
+
+typedef unsigned long long UL;
+typedef long long LL;
+typedef pair<int,int> PII;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<LL> VL;
+typedef vector<VL> VVL;
+typedef vector<PII> VPII;
+typedef vector<VPII> VVPII;
+
+#define REP(i, n) for(int i = 0; i < (n); ++i)
+#define FOR(i, st, nd) for(int i = (st); i < (nd); ++i)
+#define SET(a, val) memset(a, val, sizeof(a))
+#define mp make_pair
+
+#define INF numeric_limits<int>::max()
+#define EPS (1e-9)
+#define FLT_EQ(x,y) ((fabs((x)-(y))) < EPS)
+
+template<class T>
+struct Point {
+	T x, y;
+	Point(T x, T y) : x(x), y(y) {}
+	Point() : Point(0,0){}
+	Point<T> &operator+(Point<T> other) const {
+		other += *this;
+		return other;
+	}
+	Point<T> &operator+=(Point<T> const &other) {
+		x += other.x; y += other.y;
+		return *this;
+	}
+};
+
+void solve()
+{
+	string const directions( "NSEW" );
+	const map<string,Point<double>> dir_to_point_map {
+		{ "N", {0,1} },
+		{ "NE", {1,1} },
+		{ "E", {1,0}},
+		{"SE", {1,-1}},
+		{"S", {0,-1}},
+		{"SW", {-1,-1}},
+		{"W", {-1,0}},
+		{"NW", {-1,1}}};
+														
+	string line;
+	while( ( cin >> line ) && line != "END" )
+	{
+		cout << line << endl;
+		stringstream ss(line);
+	}
+}
+
+int main(int argc, char *argv[]) {
+#ifndef ONLINE_JUDGE
+	clock_t start = clock();
+#endif 
+	// solve something here
+	solve();
+
+#ifndef ONLINE_JUDGE
+	fprintf(stderr, "Time taken : %.5f seconds.\n",
+			((float)clock()-start)/CLOCKS_PER_SEC);
+#endif
+	return 0;
+}
