026ca3cd51f105aa20e5e54e4a0f427d2d1790dd
dantony <deepakantony@A-08E-DANTONY-0.local>
2017-02-08 01:03:02 -0800

    started interval game

---
+++ code_chef/2017_february_challenge/INTERVAL/interval_game.cxx
@@ -0,0 +1,149 @@
+/*
+  Problem: Interval game
+  Link:
+  Author: Deepak Antony - "deebee" "drunkbeast" "dAbeAst"
+  Solution: Similar to nim game.
+  First find the best solution for the interval B[M-1].
+  Then find the best solution for the interval B[M-2] using the B[M-1] solution.
+  ...
+  And go on till B[0].
+*/
+
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <cassert>
+#include <ctime>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <limits>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <map>
+#include <set>
+#include <numeric>
+
+using namespace std;
+
+typedef unsigned long long UL;
+typedef long long LL;
+typedef pair<int,int> PII;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<LL> VL;
+typedef vector<VL> VVL;
+typedef vector<PII> VPII;
+typedef vector<VPII> VVPII;
+
+#define REP(i, n) for(int i = 0; i < (n); ++i)
+#define FOR(i, st, nd) for(int i = (st); i < (nd); ++i)
+#define SET(a, val) memset(a, val, sizeof(a))
+#define mp make_pair
+
+#define INF numeric_limits<int>::max()
+#define EPS (1e-9)
+#define FLT_EQ(x,y) ((fabs((x)-(y))) < EPS)
+
+void sliding_window_sum( const VL &A, int window_size, VL &sum )
+{
+	if (A.size() < window_size) return;
+	sum.assign(A.size()-window_size+1, 0);
+	if (sum.empty()) return;
+	sum[0] = accumulate(A.begin(),A.begin()+window_size,0);
+
+	for (int i = 1; i < sum.size(); ++i)
+		sum[i] = sum[i-1] - A[i-1] + A[i+window_size-1];
+}
+
+ostream &operator<<(ostream &os, const multiset<LL> &A)
+{
+	os << "[ ";
+	for (auto it = A.rbegin(); it != A.rend(); ++it) os << *it << ", ";
+	os << "]";
+	return os;
+}
+
+void sliding_window_max( const VL &A, int window_size, VL &mx )
+{
+	if (A.size() < window_size) return;
+	mx.assign(A.size()-window_size+1, 0);
+	if (mx.empty()) return;
+	multiset<LL> mx_set(A.begin(), A.begin() + window_size);
+
+	for( int i = 0; i < mx.size(); ++i ) {
+		mx[i] = *mx_set.rbegin();
+		mx_set.erase(mx_set.find(A[i]));
+		if ((i+1) < A.size())
+			mx_set.insert(A[i+window_size]);
+	}
+}
+
+void flip_sign(LL &sign)
+{
+	if (sign > 0) sign = -1;
+	else sign = 1;
+}
+
+ostream &operator<<(ostream &os, const VL &A)
+{
+	os << "[ ";
+	for (auto val : A) os << val << ", ";
+	os << "]";
+	return os;
+}
+
+void solve(const VL &A, const VL &B)
+{
+	if (A.size() <= 2) {
+		cout << accumulate(A.begin(), A.end(), 0) << endl;
+		return;
+	}
+
+	VL sum;
+	VL mx;
+//	cout << "A: " << A << endl;
+
+	sliding_window_sum(A, B[B.size()-1], sum);
+	for (int i = B.size()-2; i >= 0; --i)
+	{
+		sliding_window_max(sum, B[i]-2-B[i+1]+1, mx);
+		//cout << "Sum: " << sum << " Max: " << mx << endl;
+		// new sum 
+		sliding_window_sum(A, B[i], sum);
+		//cout << "\tNewSum: " << sum << endl;
+		// update sum
+		for ( int j = 0; j < sum.size(); ++j )
+			sum[j] -= mx[j+1];
+	}
+	//cout << "Sum: " << sum << " Max: " << mx << endl;
+	cout << *max_element(sum.begin(), sum.end()) << endl;
+}
+
+void solve()
+{
+	int T; cin >> T;
+	while (T--) {
+		int N, M; cin >> N >> M;
+		VL A(N,0), B(M,0);
+		REP(i,N) cin >> A[i];
+		REP(i,M) cin >> B[i];
+
+		solve(A,B);
+	}
+}
+
+int main(int argc, char *argv[]) {
+#ifndef ONLINE_JUDGE
+	clock_t start = clock();
+#endif 
+	solve();
+
+#ifndef ONLINE_JUDGE
+	fprintf(stderr, "Time taken : %.5f seconds.\n",
+			((float)clock()-start)/CLOCKS_PER_SEC);
+#endif
+	return 0;
+}
