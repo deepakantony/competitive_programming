d9380e61dd6fe5793b0ca86a97cf9641f5eb914a
dantony <deepakantony@A-08E-DANTONY-0.local>
2017-02-17 21:45:32 -0800

    did not start chef & yoda

---
+++ code_chef/2017_february_challenge/CHEFYODA/chef_and_yoda.cxx
@@ -0,0 +1,58 @@
+/*
+  Problem: Chef and yoda
+  Link:
+  Author: Deepak Antony - "deebee" "drunkbeast" "dAbeAst"
+  Solution: 
+  Winner on 1st set of rules - if NxM is even then chef else yoda
+  Winner on 2nd set of rules - if N and M is even then chef else yoda
+  Probabilaty - 
+*/
+
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <cassert>
+#include <ctime>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <limits>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <map>
+
+using namespace std;
+
+typedef unsigned long long UL;
+typedef long long LL;
+typedef pair<int,int> PII;
+
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<LL> VL;
+typedef vector<VL> VVL;
+typedef vector<PII> VPII;
+typedef vector<VPII> VVPII;
+
+#define REP(i, n) for(int i = 0; i < (n); ++i)
+#define FOR(i, st, nd) for(int i = (st); i < (nd); ++i)
+#define SET(a, val) memset(a, val, sizeof(a))
+#define mp make_pair
+
+#define INF numeric_limits<int>::max()
+#define EPS (1e-9)
+#define FLT_EQ(x,y) ((fabs((x)-(y))) < EPS)
+
+int main(int argc, char *argv[]) {
+#ifndef ONLINE_JUDGE
+	clock_t start = clock();
+#endif 
+	// solve something here
+
+#ifndef ONLINE_JUDGE
+	fprintf(stderr, "Time taken : %.5f seconds.\n",
+			((float)clock()-start)/CLOCKS_PER_SEC);
+#endif
+	return 0;
+}
